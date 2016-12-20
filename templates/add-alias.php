9b1bb2e2e6bd204dfd08f260bb3e43ed577bfade
Ackeem J <ackeem.berry@gmail.com>
2016-12-20 12:52:42 -0500

    --- Further implementation design change in newAlias

--- templates/add-alias.php
+++ templates/add-alias.php
@@ -18,47 +18,107 @@
     <div id="top">
       <div class="clearfix">
         <div class="row">
-          <div class="col-md-3 animated fadeInDown">
+          <div class="col-md-2">
+            <div ng-controller="sidePanelMenuController">
+              <div class="row side-logo" ng-click="goToHome()">
+                <div class="col-md-12">
+                  <img style="margin-top: -20px;" class="animated rotateIn" src="images/opal_logo_transparent_purple.png" height="130" width="130">
+                  <h1 class="animated fadeIn">opal <strong>ADMIN</strong> </h1>
+                </div>
+              </div>   
+            </div>  
+            <div class="row">
+              <div class="side-menu-title">
+                <div class="horz-line" style="height: 10px; text-align: center">
+                  <span style="background-color:#e6edfa; padding: 0 10px;">
+                    <h2>Summary</h2>
+                  </span>  
+                </div>  
+              </div>
+            </div>
+            <div class="row">
+              <div class="progress progress-striped active">
+                <div class="progress-bar" ng-class="{'progress-bar-success': stepProgress == 100}" role="progressbar" aria-valuenow="{{stepProgress}}" aria-valuemin="0" aria-valuemax="100" style="width: {{stepProgress}}%">
+                </div>
+              </div>
+            </div>
             <div class="panel-container">
               <div class="panel-info">
-                <div class="panel-title-custom" style="border-bottom: 0">
-                  <h2 style="margin-bottom: 15px; margin-top: 0;">
-                    <span style="font-size: 20px;" class="glyphicon glyphicon-th-list" aria-hidden="true"></span>
-                    Progress: {{stepProgress}}% Complete
-                  </h2>
-                  <div class="progress progress-striped active">
-                    <div class="progress-bar" ng-class="{'progress-bar-success': stepProgress == 100}" role="progressbar" aria-valuenow="{{stepProgress}}" aria-valuemin="0" aria-valuemax="100" style="width: {{stepProgress}}%">
-                    </div>
-                  </div>
-                </div>
                 <div class="panel-content" style="padding-top: 0">
-                  <ul class="list-group">
-                    <li class="list-group-item" ng-show="newAlias.source_db">
-                      <strong>Source Database:</strong> {{newAlias.source_db.name}}
-                    </li>
-                    <li class="list-group-item" ng-show="newAlias.name_EN || newAlias.name_FR">
-                      <strong>Titles:</strong> EN: {{newAlias.name_EN}} | FR: {{newAlias.name_FR}}
-                    </li>
-                    <li class="list-group-item" ng-show="newAlias.description_EN || newAlias.description_FR">
-                      <strong>Descriptions:</strong> EN: {{newAlias.description_EN}} | FR: {{newAlias.description_FR}}
-                    </li>
-                    <li class="list-group-item" ng-show="newAlias.eduMat">
-                      <strong>Educational Material:</strong> {{newAlias.eduMat.name_EN}}
-                    </li>
-                    <li class="list-group-item" ng-show="newAlias.type">
-                      <strong>Type:</strong> {{newAlias.type}}
-                    </li>
-                    <li class="list-group-item" ng-show="checkTermsAdded(termList)"> 
-                      <strong>Term(s):</strong>
-                      <p style="margin-top: 10px;">
-                        <ul style="max-height: 100px; overflow-y: auto;">
-                          <li ng-repeat="term in termList | filter: {added: true} : true">
-                            {{term.name}}
-                          </li>
-                        </ul>
-                      </p>
-                    </li>
-                  </ul>
+                  <div class="summary-box">
+                    <div>
+                      <div class="horz-line" style="height: 10px;">
+                        <span class="title" style="background-color: #fff; margin-left: 5px;">
+                          <strong>Source Database</strong>
+                        </span>
+                      </div>
+                      <div class="description">
+                        <p ng-show="newAlias.source_db.name">{{newAlias.source_db.name}}</p>
+                      </div>
+                    </div>
+                    <div>
+                      <div class="horz-line" style="height: 10px;">
+                        <span class="title" style="background-color: #fff; margin-left:5px;">
+                          <strong>Titles & Description</strong>
+                        </span>
+                      </div>
+                      <div class="description">
+                        <p ng-show="newAlias.name_EN"><strong>EN: </strong>{{newAlias.name_EN}}</p>
+                        <p ng-show="newAlias.description_EN"><em>{{newAlias.description_EN}}</em></p>
+                      </div>
+                      <div class="description">
+                        <p ng-show="newAlias.name_FR"><strong>FR: </strong>{{newAlias.name_FR}}</p>
+                        <p ng-show="newAlias.description_FR"><em>{{newAlias.description_FR}}</em></p>
+                      </div>
+                    </div>  
+                    <div>
+                      <div class="horz-line" style="height: 10px;">
+                        <span class="title" style="background-color: #fff; margin-left: 5px;">
+                          <strong>Educational Material</strong>
+                        </span>
+                      </div>
+                      <div class="description">
+                        <p ng-show="newAlias.eduMat">{{newAlias.eduMat.name_EN}}</p>
+                      </div>
+                    </div>
+                    <div>
+                      <div class="horz-line" style="height: 10px;">
+                        <span class="title" style="background-color: #fff; margin-left: 5px;">
+                          <strong>Type</strong>
+                        </span>
+                      </div>
+                      <div class="description">
+                        <p ng-show="newAlias.type">{{newAlias.type}}</p>
+                      </div>
+                    </div>
+                    <div>
+                      <div class="horz-line" style="height: 10px;">
+                        <span class="title" style="background-color: #fff; margin-left: 5px;">
+                          <strong>Icon Color</strong>
+                        </span>
+                      </div>
+                      <div class="description"> 
+                        <div style="display:inline-block" ng-show="newAlias.color" class="color-palette" ng-style="{'background-color': newAlias.color}"></div>
+                        <p style="display: inline" ng-show="newAlias.color">{{newAlias.color}}</p>
+                      </div>
+                    </div>
+                    <div>
+                      <div class="horz-line" style="height: 10px;">
+                        <span class="title" style="background-color: #fff; margin-left: 5px;">
+                          <strong>Aliased Expressions</strong>
+                        </span>
+                      </div>
+                      <div class="description">
+                        <p ng-show="checkTermsAdded(termList)">
+                          <ul style="max-height: 100px; overflow-y: auto;">
+                            <li ng-repeat="term in termList | filter: {added: true} : true">
+                              {{term.name}}
+                            </li>
+                          </ul>
+                        </p>
+                      </div>
+                    </div>
+                  </div>  
                   <div ng-hide="toggleAlertText()" class="table-buttons" style="text-align: center">
                     <form ng-submit="submitAlias()" method="post">
                       <button class="btn btn-primary" ng-class="{'disabled': !checkForm()}" type="submit">Submit</button>
@@ -77,7 +137,178 @@
               </div>
             </div>
           </div>
-          <div class="col-md-9 animated fadeInRight">
+          <div class="col-md-10 animated fadeInRight">
+            <div class="row main-title">
+              <div class="col-md-6 title-content">
+                <span class="glyphicon glyphicon-plus"></span>
+                <h1><strong>Add Alias</strong></h1>
+              </div>
+              <div class="col-md-6 title-breadcrumbs"> 
+                <span>
+                  <span>Home</span>
+                  <span class="teflon glyphicon glyphicon-menu-right"></span> 
+                  <span>Aliases</span>
+                  <span class="teflon glyphicon glyphicon-menu-right"></span> 
+                  <span><strong>Add Alias</strong></span>
+                </span>
+              </div>
+            </div>    
+            <div class="row">
+              <div class="col-md-10 side-menu-title">
+                <div style="height: 10px; border-bottom: 1px solid #6f5499;">
+                  <span style="background-color:#e6edfa; padding: 0 10px;">
+                    <span class="glyphicon glyphicon-menu-down"></span>
+                    <h2>Source Database</h2>
+                  </span>  
+                </div>  
+              </div>
+            </div>   
+            <div class="row">
+              <div class="col-md-10">
+                <p style="margin-bottom: 10px;">
+                  <span style="color:#d9534f"><strong>Required field:</strong></span>
+                  Please select a source database.
+                </p>
+              </div>
+            </div>    
+            <div class="row">
+              <div ng-repeat="sourceDB in sourceDBList" class="col-md-1"> 
+                <div class="panel-container animated" style="cursor:pointer;" ng-class="{pulse: hover, active: newAlias.source_db.name == sourceDB.name}" ng-click="sourceDBUpdate(sourceDB)" ng-mouseenter="hover=true" ng-mouseleave="hover=false">
+                  <div class="panel-info" ng-class="{active: newAlias.source_db.name == sourceDB.name}">
+                    <div class="panel-content" style="text-align:center">
+                      <span style="font-size:30px;" class="fa fa-database"></span>
+                      <div class="option-panel-title">{{sourceDB.name}}</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>          
+            <div class="row">
+              <div class="col-md-10 side-menu-title">
+                <div style="height: 10px; border-bottom: 1px solid #6f5499;">
+                  <span style="background-color:#e6edfa; padding: 0 10px;">
+                    <span class="glyphicon glyphicon-menu-down"></span>
+                    <h2>Title & Description</h2>
+                  </span>  
+                </div>  
+              </div>
+            </div>   
+            <div class="row">
+              <div class="col-md-10">
+                <p style="margin-bottom: 10px;">
+                  <span style="color:#d9534f"><strong>Required field:</strong></span>
+                  Please assign an english and french title and description.
+                </p>
+              </div>
+            </div>  
+            <div class="row"> 
+              <div class="col-md-6">
+                <div class="panel" ng-class="(newAlias.name_EN && newAlias.description_EN) ? 'panel-success': 'panel-danger'">
+                  <div class="panel-heading"><strong>English</strong>
+                    <span ng-hide="newAlias.name_EN && newAlias.description_EN" style="float:right"><em>Incomplete</em></span>
+                    <span ng-show="newAlias.name_EN && newAlias.description_EN" style="float:right"><em>Complete</em></span>
+                  </div>
+                  <div class="panel-body">
+                    <div class="row">
+                      <div class="col-md-2">
+                        <div style="font-size:18px; text-align:right;">Title</div>
+                      </div>
+                      <div class="col-md-10">
+                        <div class="form-group">
+                          <input class="form-control" type="text" ng-model="newAlias.name_EN" ng-change="titleUpdate()" placeholder="English Title" required="required">
+                        </div>
+                      </div>
+                    </div>    
+                    <div class="row">
+                      <div class="col-md-2">
+                        <div style="font-size:18px; text-align:right;">Description</div>
+                      </div>
+                      <div class="col-md-10">
+                        <div class="form-group">
+                          <textarea class="form-control" rows="5" ng-model="newAlias.description_EN" ng-change="descriptionUpdate()" placeholder="English Description" required="required"></textarea>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>          
+              <div class="col-md-6">
+                <div class="panel" ng-class="(newAlias.name_FR && newAlias.description_FR) ? 'panel-success': 'panel-danger'">
+                  <div class="panel-heading"><strong>Français</strong>
+                    <span ng-hide="newAlias.name_FR && newAlias.description_FR" style="float:right"><em>Incomplete</em></span>
+                    <span ng-show="newAlias.name_FR && newAlias.description_FR" style="float:right"><em>Complete</em></span>
+                  </div>
+                  <div class="panel-body">
+                    <div class="row">
+                      <div class="col-md-2">
+                        <div style="font-size:18px; text-align:right;">Titre</div>
+                      </div>
+                      <div class="col-md-10">
+                        <div class="form-group">
+                          <input class="form-control" type="text" ng-model="newAlias.name_FR" ng-change="titleUpdate()" placeholder="Titre Français" required="required">
+                        </div>
+                      </div>
+                    </div>    
+                    <div class="row">
+                      <div class="col-md-2">
+                        <div style="font-size:18px; text-align:right;">Description</div>
+                      </div>
+                      <div class="col-md-10">
+                        <div class="form-group">
+                          <textarea class="form-control" rows="5" ng-model="newAlias.description_FR" ng-change="descriptionUpdate()" placeholder="Description Français" required="required"></textarea>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>          
+            </div>
+            <div class="row">
+              <div class="col-md-10 side-menu-title">
+                <div style="height: 10px; border-bottom: 1px solid #6f5499;">
+                  <span style="background-color:#e6edfa; padding: 0 10px;">
+                    <span class="glyphicon glyphicon-menu-down"></span>
+                    <h2>Educational Material</h2>
+                  </span>  
+                </div>  
+              </div>
+            </div>   
+            <div class="row">
+              <div class="col-md-10">
+                <p style="margin-bottom: 10px;">
+                  <span><strong>Optional field:</strong></span>
+                  Assigning an educational material will provide a rich-text description of the alias.
+                </p>
+              </div>
+            </div>  
+            <div class="row"> 
+              <div class="col-md-8">
+                <div class="panel-container">
+                  <div class="panel-info">
+                    <div class="panel-content">
+                      <div class="panel-input">  
+                        <div class="list-space">
+                          <div class="input-group">
+                            <span class="input-group-addon"><span class="glyphicon glyphicon-search"></span></span>
+                            <input class="form-control" type="text" ng-model="eduMatFilter" ng-change="changeEduMatFilter(eduMatFilter)" placeholder="Search Educational Material"/>
+                          </div>
+                          <ul class="list-items">
+                            <li ng-repeat="eduMat in eduMatList | filter: searchEduMatsFilter">
+                              <label>
+                                <input type="radio" ng-model="newAlias.eduMat" ng-change="eduMatUpdate()" ng-value="eduMat" /> {{eduMat.name_EN}}
+                              </label>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+ 
+                    
+
             <div class="panel-container" style="text-align: left">
               <uib-accordion close-others="true">
                 <uib-accordion-group ng-class="newAlias.source_db ? 'panel-success': 'panel-danger'" is-open="true">
@@ -95,7 +326,7 @@
                         </label>
                       </li>
                     </ul>
-                  </div>
+                  </ div>
                 </uib-accordion-group>     
                 <uib-accordion-group ng-class="(newAlias.name_EN && newAlias.name_FR) ? 'panel-success': 'panel-danger'" is-open="statusA">
                   <uib-accordion-heading> 
