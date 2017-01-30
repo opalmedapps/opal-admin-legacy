8d0ceb9f810afa8c4f85a0c2dab18b80ea6c6aec
Ackeem Joseph <ackeem.berry@gmail.com>
2017-01-30 12:29:14 -0500

    --- File location needs to be specified in update_crontab.php file since     it's being called remotely.

--- php/cron/update_crontab.php
+++ php/cron/update_crontab.php
@@ -2,6 +2,15 @@
 
 	/* To update crontab when this file is called from the command line */
 
+	/* To update crontab when this file is called from the command line */
+
+    $currentFile = __FILE__; // Get location of this script
+
+    // Find config file based on this location 
+    $configFile = substr($currentFile, 0, strpos($currentFile, "opalAdmin")) . "opalAdmin/php/config.php";
+	// Include config file 
+    include_once($configFile);
+
 	$cron = new CronControl; // Object
 
 	// The argument pass is the cronSer
