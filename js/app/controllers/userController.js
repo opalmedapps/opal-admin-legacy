6bd8adb99e99015e889d027358e1194262dd2b72
Ackeem Joseph <ackeem.berry@gmail.com>
2017-02-03 10:27:26 -0500

    --- Started adding users page functionality

---
+++ js/app/controllers/userController.js
@@ -0,0 +1,10 @@
+angular.module('opalAdmin.controllers.userController', ['ui.bootstrap', 'ui.grid']).
+
+
+	/******************************************************************************
+	* Controller for the users page
+	*******************************************************************************/
+	controller('userController', function($scope, $uibModal) {
+					
+	});
+
