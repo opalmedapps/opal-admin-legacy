184bc3b74d4bffed37a3f88819813ca69c82f694
Ackeem J <ackeem.berry@gmail.com>
2017-03-06 11:52:04 -0500

    --- Fixed issue where site would crash if Aria was not defined         -- Implemented the same source database check as in the backend, so the proper         source database information would load (if enabled)
        --- Updated schema to include test Opal patients

--- php/classes/Filter.php
+++ php/classes/Filter.php
@@ -19,35 +19,191 @@ class Filter {
             'doctors'       => array(),
             'resources'     => array()
         );
-        try {
-	        $aria_link = new PDO( ARIA_DB_DSN , ARIA_DB_USERNAME, ARIA_DB_PASSWORD );
-            $aria_link->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $databaseObj = new Database();
 
-			$host_db_link = new PDO( OPAL_DB_DSN, OPAL_DB_USERNAME, OPAL_DB_PASSWORD );
-            $host_db_link->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-        
-            $sql = "
-                SELECT DISTINCT
-                    vva.Expression1
-                FROM   
-                    variansystem.dbo.vv_ActivityLng vva
-                ORDER BY
-                    vva.Expression1
-            ";
-            $query = $aria_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
-            $query->execute();
-            while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-         
-                $expressionArray = array(
-                    'name'  => $data[0],
-                    'id'    => $data[0],
-                    'type'  => 'Expression',
-                    'added' => 0
-                );
-                array_push($filters['expressions'], $expressionArray);
+        try {
+
+            // ***********************************
+            // ARIA 
+            // ***********************************
+            $sourceDBSer = 1;
+            $source_db_link = $databaseObj->connectToSourceDatabase($sourceDBSer);
+            if ($source_db_link) {
+		
+                $sql = "
+                    SELECT DISTINCT
+                        vva.Expression1
+                    FROM   
+                        variansystem.dbo.vv_ActivityLng vva
+                    ORDER BY
+                        vva.Expression1
+                ";
+                $query = $source_db_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
+                $query->execute();
+                while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+             
+                    $expressionArray = array(
+                        'name'  => $data[0],
+                        'id'    => $data[0],
+                        'type'  => 'Expression',
+                        'added' => 0
+                    );
+                    array_push($filters['expressions'], $expressionArray);
+                }
+
+                $sql = "
+                    SELECT DISTINCT
+                        Doctor.ResourceSer,
+                        Doctor.LastName
+                    FROM
+                        variansystem.dbo.Doctor Doctor,
+                        variansystem.dbo.PatientDoctor PatientDoctor
+                    WHERE 
+                        PatientDoctor.PrimaryFlag       = 1
+                    AND PatientDoctor.OncologistFlag    = 1
+                    AND Doctor.OncologistFlag           = 1
+                    AND PatientDoctor.ResourceSer       = Doctor.ResourceSer
+
+                    ORDER BY
+                        Doctor.LastName
+                ";
+                $query = $source_db_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
+                $query->execute();
+                while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                    $doctorArray = array(
+                        'name'  => $data[1],
+                        'id'    => $data[0],
+                        'type'  => 'Doctor',
+                        'added' => 0
+                    );
+                    array_push($filters['doctors'], $doctorArray);
+                }
+
+                $sql = "
+                    SELECT DISTINCT
+                        vr.ResourceSer,
+                        vr.ResourceName
+                    FROM    
+                        variansystem.dbo.vv_ResourceName vr
+                    WHERE
+                        vr.ResourceName     LIKE 'STX%'
+                    OR  vr.ResourceName     LIKE 'TB%'
+
+                    ORDER BY 
+                        vr.ResourceName
+                ";
+                $query = $source_db_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
+                $query->execute();
+                while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                    $resourceArray = array(
+                        'name'  => $data[1],
+                        'id'    => $data[0],
+                        'type'  => 'Resource',
+                        'added' => 0
+                    );
+                    array_push($filters['resources'], $resourceArray);
+                }
             }
 
-            $dxTranslations = array();
+            // ***********************************
+            // WaitRoomManagement 
+            // ***********************************
+            $sourceDBSer = 2;
+            $source_db_link = $databaseObj->connectToSourceDatabase($sourceDBSer);
+            if ($source_db_link) {
+        
+                $sql = "SELECT 'EXPRESSION_QUERY_HERE'";
+                // $query = $source_db_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
+                // $query->execute();
+                // while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+             
+                    // $expressionArray = array(
+                    //     'name'  => $data[0],
+                    //     'id'    => $data[0],
+                    //     'type'  => 'Expression',
+                    //     'added' => 0
+                    // );
+                    // array_push($filters['expressions'], $expressionArray);
+                //}
+
+                $sql = "SELECT 'DOCTOR_QUERY_HERE'";
+                // $query = $source_db_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
+                // $query->execute();
+                // while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                //     $doctorArray = array(
+                //         'name'  => $data[1],
+                //         'id'    => $data[0],
+                //         'type'  => 'Doctor',
+                //         'added' => 0
+                //     );
+                //     array_push($filters['doctors'], $doctorArray);
+                // }
+
+                $sql = "SELECT 'RESOURCE_QUERY_HERE'";
+                // $query = $source_db_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
+                // $query->execute();
+                // while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                //     $resourceArray = array(
+                //         'name'  => $data[1],
+                //         'id'    => $data[0],
+                //         'type'  => 'Resource',
+                //         'added' => 0
+                //     );
+                //     array_push($filters['resources'], $resourceArray);
+                // }
+            }
+
+            // ***********************************
+            // Mosaiq 
+            // ***********************************
+            $sourceDBSer = 3;
+            $source_db_link = $databaseObj->connectToSourceDatabase($sourceDBSer);
+            if ($source_db_link) {
+        
+                $sql = "SELECT 'EXPRESSION_QUERY_HERE'";
+                // $query = $source_db_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
+                // $query->execute();
+                // while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+             
+                    // $expressionArray = array(
+                    //     'name'  => $data[0],
+                    //     'id'    => $data[0],
+                    //     'type'  => 'Expression',
+                    //     'added' => 0
+                    // );
+                    // array_push($filters['expressions'], $expressionArray);
+                //}
+
+                $sql = "SELECT 'DOCTOR_QUERY_HERE'";
+                // $query = $source_db_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
+                // $query->execute();
+                // while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                //     $doctorArray = array(
+                //         'name'  => $data[1],
+                //         'id'    => $data[0],
+                //         'type'  => 'Doctor',
+                //         'added' => 0
+                //     );
+                //     array_push($filters['doctors'], $doctorArray);
+                // }
+
+                $sql = "SELECT 'RESOURCE_QUERY_HERE'";
+                // $query = $source_db_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
+                // $query->execute();
+                // while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+                //     $resourceArray = array(
+                //         'name'  => $data[1],
+                //         'id'    => $data[0],
+                //         'type'  => 'Resource',
+                //         'added' => 0
+                //     );
+                //     array_push($filters['resources'], $resourceArray);
+                // }
+            }
+
+            $host_db_link = new PDO( OPAL_DB_DSN, OPAL_DB_USERNAME, OPAL_DB_PASSWORD );
+            $host_db_link->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        
             $sql = "
                 SELECT DISTINCT
                     dt.AliasName
@@ -68,61 +224,9 @@ class Filter {
                 array_push($filters['dx'], $dxArray);
             }
 
-            $sql = "
-                SELECT DISTINCT
-                    Doctor.ResourceSer,
-                    Doctor.LastName
-                FROM
-                    variansystem.dbo.Doctor Doctor,
-                    variansystem.dbo.PatientDoctor PatientDoctor
-                WHERE 
-                    PatientDoctor.PrimaryFlag       = 1
-                AND PatientDoctor.OncologistFlag    = 1
-                AND Doctor.OncologistFlag           = 1
-                AND PatientDoctor.ResourceSer       = Doctor.ResourceSer
-
-                ORDER BY
-                    Doctor.LastName
-            ";
-            $query = $aria_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
-            $query->execute();
-            while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-                $doctorArray = array(
-                    'name'  => $data[1],
-                    'id'    => $data[0],
-                    'type'  => 'Doctor',
-                    'added' => 0
-                );
-                array_push($filters['doctors'], $doctorArray);
-            }
-
-            $sql = "
-                SELECT DISTINCT
-                    vr.ResourceSer,
-                    vr.ResourceName
-                FROM    
-                    variansystem.dbo.vv_ResourceName vr
-                WHERE
-                    vr.ResourceName     LIKE 'STX%'
-                OR  vr.ResourceName     LIKE 'TB%'
-
-                ORDER BY 
-                    vr.ResourceName
-            ";
-            $query = $aria_link->prepare( $sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL) );
-            $query->execute();
-            while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
-                $resourceArray = array(
-                    'name'  => $data[1],
-                    'id'    => $data[0],
-                    'type'  => 'Resource',
-                    'added' => 0
-                );
-                array_push($filters['resources'], $resourceArray);
-            }
-
             return $filters;
-        	} catch (PDOException $e) {
+
+        } catch (PDOException $e) {
 			echo $e->getMessage();
 			return $filters;
 		}
