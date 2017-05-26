ec08f00ea58c8d56ad59fa7365b2b979c240211b
Qi <qi@radoncreports.(none)>
2017-05-26 15:07:59 -0400

    --- Committing on behalf of Qi (AJ)

---
+++ php/classes/Questionnaire classes/Question.php
@@ -0,0 +1,58 @@
+<?php
+
+/* Questionnaire-Question class */
+
+class Question {
+	public $serNum;
+	public $question;
+	public $type;
+	public $selected;
+
+	public function __construct($serNum, $question, $type, $selected){
+		$this->serNum = $serNum;
+		$this->question = $question;
+		$this->type = $type;
+		$this->selected = $selected;
+	}
+
+	/* Add new question into table.
+	 * @param: $questionAdded-array that contains necessary infromation for a question to be stored into table
+	 */
+ 	public function addQuestion($questionAdded){
+ 		// properties of a question
+		$text_EN = $questionAdded['text_EN'];
+		$text_FR = $questionAdded['text_FR'];
+		$answertype_serNum = $questionAdded['answertype_serNum'];
+		$questiongroup_serNum = $questionAdded['questiongroup_serNum'];
+		$created_by = $questionAdded['created_by'];
+
+		try {
+			$host_db_link = new PDO( OPAL_DB_DSN, OPAL_DB_USERNAME, OPAL_DB_PASSWORD );
+			$host_db_link->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			$sql = "
+				INSERT INTO
+					QuestionnaireQuestion (
+						text_EN,
+						text_FR,
+						questiongroup_serNum,
+						answertype_serNum,
+						created_by
+					)
+				VALUES (
+					\"$text_EN\",
+					\"$text_FR\",
+					'$questiongroup_serNum',
+					'$answertype_serNum',
+					'$created_by'
+				)
+			";
+
+			$query = $host_db_link->prepare( $sql );
+			$query->execute();
+
+		} catch( PDOException $e) {
+			return $e->getMessage();
+		}
+	}
+}
+?>
---
+++ php/classes/Questionnaire classes/Questionnaire.php
@@ -0,0 +1,169 @@
+<?php
+
+/* Questionnaire class 
+ * Dependencies: Questionanire-Tag
+ */
+include_once('Tag.php');
+
+class Questionnaire{
+	public $serNum;
+	public $name;
+	public $private;
+	public $publish;
+	public $created_by;
+	public $tags;
+
+	public function __construct($serNum, $name, $private, $publish, $created_by){
+		$this->serNum = $serNum;
+		$this->name = $name;
+		$this->private = $private;
+		$this->publish = $publish;
+		$this->created_by = $created_by;
+		$this->tags = array();
+	}
+
+	public function addTag($tag){
+		array_push($this->tags,$tag);
+	}
+
+	/* Get Questionnaire
+	 * @return questionnaires as array
+	 */
+	public function getQuestionnaire($post){
+		$doctor_id = $post['doctorSerNum'];
+		$questionnaires = array();
+		try {
+			$host_db_link = new PDO( OPAL_DB_DSN, OPAL_DB_USERNAME, OPAL_DB_PASSWORD );
+			$host_db_link->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+			$sql = "
+				SELECT
+					serNum,
+					name_EN,
+					private,
+					publish,
+					created_by
+				FROM
+					Questionnaire
+				WHERE
+					private = 0
+				OR
+					created_by ="
+					. $doctor_id . 
+					";";
+
+			$query = $host_db_link->prepare( $sql );
+			$query->execute();
+
+			// fetch
+			while ($data = $query->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+
+	 			$serNum 	= $data[0];
+	 			$name_EN   	= $data[1];
+	 			$private 	= $data[2];
+	 			$publish 	= $data[3];
+	 			$created_by = $data[4];
+
+	 			$questionnaireObj = new Questionnaire($serNum, $name_EN, $private, $publish, $created_by);
+	 			array_push($questionnaires, $questionnaireObj);
+
+	 			// get tags
+				$tagsql = "
+					SELECT
+						tag_serNum,
+						name_EN
+					FROM
+						Questionnaire_tag,
+						QuestionnaireTag
+					WHERE
+						Questionnaire_tag.tag_serNum = QuestionnaireTag.serNum
+					AND
+						Questionnaire_tag.questionnaire_serNum ="
+						. $data['serNum'] . 
+						";";
+
+				$tagquery = $host_db_link->prepare( $tagsql );
+				$tagquery->execute();
+
+				while ($tagdata = $tagquery->fetch(PDO::FETCH_NUM, PDO::FETCH_ORI_NEXT)) {
+					$tag_serNum = $tagdata[0];
+					$tagName_EN = $tagdata[1];
+
+					$tagObj = new Tag($tag_serNum, $tagName_EN);
+					$questionnaireObj->addTag($tagObj);
+				}
+	 		} catch (PDOException $e) {
+	 			echo $e->getMessage();
+	 			return $questionnaires;
+	 		}
+		}
+	}
+
+	public function createQuestionnaire($post){
+		// properties
+		$name_EN = $post['name_EN'];
+		$name_FR = $post['name_FR'];
+		$private = $post['private'];
+		$publish = $post['publish'];
+		$created_by = $post['created_by'];
+		$tags = $post['tags'];
+		$questiongroups = $post['questionGroups']
+
+		try {
+			$host_db_link = new PDO( OPAL_DB_DSN, OPAL_DB_USERNAME, OPAL_DB_PASSWORD );
+			$host_db_link->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+
+			$sql = "
+				INSERT INTO
+					Questionnaire(
+						name_EN,
+						name_FR,
+						private,
+						publish,
+						created_by
+					)
+				VALUES(
+					\"$name_EN\",
+					\"$name_FR\",
+					$private,
+					$publish,
+					$created_by
+				)
+			";
+
+			$query = $host_db_link->prepare( $sql );
+			$query->execute();
+
+			$questionnaire_id =  $host_db_link->lastInsertId();
+			//position counter
+			$i = 0;
+			foreach($questiongroups as $group){
+				$group_id = $group->serNum;
+				$optional = $group->optional;
+
+				$sql = "
+					INSERT INTO
+						Questionnaire_questiongroup(
+							questionnaire_serNum,
+							questiongroup_serNum,
+							position
+						)
+					VALUES(
+						$questionnaire_id,
+						$group_id,
+						$i
+					)
+				";
+				i++;
+			}
+		} catch( PDOException $e) {
+			return $e->getMessage();
+		}
+	}
+
+	
+	public function addGroupToQuestionnaire($post){
+
+	}
+}
+?>
