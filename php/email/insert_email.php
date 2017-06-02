ca4696fc0d0a536d619decbab15f16c2c50b30f7
Ackeem Joseph <ackeem.berry@gmail.com>
2017-06-02 16:50:11 -0400

    --- Added insert, remove, update email

---
+++ php/email/insert_email.php
@@ -0,0 +1,20 @@
+<?php
+
+	/* To insert a newly created email template */
+	include_once('email.inc');
+
+	// Construct array
+	$emailArray	= array(
+		'name_EN' 	        => $_POST['name_EN'],
+		'name_FR' 	        => $_POST['name_FR'],
+        'body_EN'           => str_replace(array('"', "'"), '\"', $_POST['body_EN']),
+        'body_FR'           => str_replace(array('"', "'"), '\"', $_POST['body_FR']),
+ 		'type' 		        => $_POST['type']
+	);
+
+	$emailObject = new Email; // Object
+
+	// Call function
+	$emailObject->insertEmail($emailArray);
+	
+?>
