8ce0b9bddfb8ed9e2238037dac6f7be83365ea78
Ackeem Joseph <ackeem.berry@gmail.com>
2017-11-22 11:58:19 -0500

    --- Fixed crash caused by invalid column name

--- publisher/modules/Task.pm
+++ publisher/modules/Task.pm
@@ -348,7 +348,7 @@ sub getTasksFromSourceDB
 						variansystem.dbo.NonScheduledActivity NonScheduledActivity
                     LEFT JOIN variansystem.dbo.NonScheduledActivityMH NonScheduledActivityMH
                     ON  NonScheduledActivityMH.NonScheduledActivitySer = NonScheduledActivity.NonScheduledActivitySer
-                    AND NonScheduledActivityMH.RevCount = (
+                    AND NonScheduledActivityMH.NonScheduledActivityRevCount = (
                         SELECT MIN(nsamh.NonScheduledActivityRevCount)
                         FROM variansystem.dbo.NonScheduledActivityMH nsamh
                         WHERE nsamh.NonScheduledActivitySer = NonScheduledActivity.NonScheduledActivitySer
@@ -393,6 +393,8 @@ sub getTasksFromSourceDB
 						$taskInfo_sql .= ")";
 					}
 	        	}
+                
+                print "$taskInfo_sql\n";
 
 	        	# prepare query
     		    my $query = $sourceDatabase->prepare($taskInfo_sql)
