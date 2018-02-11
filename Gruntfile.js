c00756e765dd462d505c608bf99898572f86bcb7
Ackeem Joseph <ackeem.berry@gmail.com>
2018-02-11 17:37:26 -0500

    --- Setting up for versioning

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,25 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-bump');
 
 	grunt.initConfig({
-	  bump: {
-	    options: {
-	      files: ['package.json'],
-	      updateConfigs: [],
-	      commit: false,
-	      commitMessage: 'Release v%VERSION%',
-	      commitFiles: ['package.json'],
-	      createTag: false,
-	      tagName: 'v%VERSION%',
-	      tagMessage: 'Version %VERSION%',
-	      push: false,
-	      pushTo: 'upstream',
-	      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-	      globalReplace: false,
-	      prereleaseName: 'rc',
-	      metadata: '',
-	      regExp: false
-	    }
-	  },
+		bump: {
+			options: {
+				files: ['package.json', 'config.json'],
+				updateConfigs: [],
+				commit: false,
+				commitMessage: 'Release v%VERSION%',
+				commitFiles: ['package.json'],
+				createTag: false,
+				tagName: 'v%VERSION%',
+				tagMessage: 'Version %VERSION%',
+				push: false,
+				pushTo: 'upstream',
+				gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+				globalReplace: false,
+				prereleaseName: 'rc',
+				metadata: '',
+				regExp: false
+			}
+		},
 	});
 };
