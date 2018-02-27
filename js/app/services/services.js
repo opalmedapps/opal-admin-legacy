459e60c2ba2704e81e93df1322c5cbc120eb46c9
Ackeem J <ackeem.berry@gmail.com>
2018-02-27 16:53:33 -0500

    --- Further developments to filter

--- js/app/services/services.js
+++ js/app/services/services.js
@@ -38,4 +38,162 @@ angular.module('opalAdmin.services', [])
 			Session.destroy();
 			$state.go('login');
 		};
+	})
+
+	.service('FrequencyFilterService', function () {
+		this.presetFrequencies = [
+		{
+			name: 'Once',
+			id: 'once',
+			meta_key: 'repeat_day',
+			meta_value: 0
+		},{
+			name: 'Every Day',
+			id: 'every_day',
+			meta_key: 'repeat_day',
+			meta_value: 1
+		},{
+			name: 'Every Other Day',
+			id: 'every_other_day',
+			meta_key: 'repeat_day',
+			meta_value: 2
+		},{
+			name: 'Every Week',
+			id: 'every_week',
+			meta_key: 'repeat_week',
+			meta_value: 1
+		},{
+			name: 'Every 2 Weeks',
+			id: 'every_2_weeks',
+			meta_key: 'repeat_week',
+			meta_value: 2
+		},{
+			name: 'Every Month',
+			id: 'every_month',
+			meta_key: 'repeat_month',
+			meta_value: 1
+		},{
+			name: 'Custom',
+			id: 'custom',
+			meta_key: null,
+			meta_value: null
+		}];
+
+		this.customFrequency = {
+			meta_value: 1,
+			unit:null
+		}
+
+		this.frequencyUnits = [
+		{
+			name: 'Days',
+			id: 'day',
+			meta_key: 'repeat_day'
+		},{
+			name: 'Weeks',
+			id: 'week',
+			meta_key: 'repeat_week'
+		},{
+			name: 'Months',
+			id: 'month',
+			meta_key: 'repeat_month'
+		},{
+			name: 'Years',
+			id: 'year',
+			meta_key: 'repeat_year'
+		}];
+
+		this.daysInWeek = [
+		{
+			name: 'Sunday',
+			id: 1
+		},{
+			name: 'Monday',
+			id: 2
+		},{
+			name: 'Tuesday',
+			id: 3
+		},{
+			name: 'Wednesday',
+			id: 4
+		},{
+			name: 'Thursday',
+			id: 5
+		},{
+			name: 'Friday',
+			id: 6
+		},{
+			name: 'Saturday',
+			id: 7
+		}];
+
+		this.weekNumbersInMonth = [
+		{
+			name: '---',
+			id: null
+		},{
+			name: '1st',
+			id: 1
+		},{
+			name: '2nd',
+			id: 2
+		},{
+			name: '3rd',
+			id: 3
+		},{
+			name: '4th',
+			id: 4
+		},{
+			name: '5th',
+			id: 5
+		},{
+			name: 'Last',
+			id: 6
+		}];
+
+		this.monthsInYear = [
+		{
+			name: 'January',
+			id: 1
+		},{
+			name: 'February',
+			id: 2
+		},{
+			name: 'March',
+			id: 3
+		},{
+			name: 'April',
+			id: 4
+		},{
+			name: 'May',
+			id: 5
+		},{
+			name: 'June',
+			id: 6
+		},{
+			name: 'July',
+			id: 7
+		},{
+			name: 'August',
+			id: 8
+		},{
+			name: 'September',
+			id: 9
+		},{ 
+			name: 'October',
+			id: 10
+		},{
+			name: 'November',
+			id: 11
+		},{
+			name: 'December',
+			id: 12
+		}];
+
+		this.additionalMeta = {
+			repeat_day_iw: [],
+			repeat_week_im: [],
+			repeat_date_im: [],
+			repeat_month_iy: []
+		};
 	});
