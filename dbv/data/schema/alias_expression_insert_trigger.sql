00525b90d8466916e29a41ffab7f56dc30bd125c
Ackeem Joseph <ackeem.berry@gmail.com>
2018-05-08 09:41:07 -0400

    --- Added triggers and revisions

---
+++ dbv/data/schema/alias_expression_insert_trigger.sql
@@ -0,0 +1,4 @@
+CREATE DEFINER=`ackeem`@`%` TRIGGER `alias_expression_insert_trigger` AFTER INSERT ON `AliasExpression`
+ FOR EACH ROW BEGIN
+   INSERT INTO `AliasExpressionMH`(`AliasSerNum`, `ExpressionName`, `Description`, `LastTransferred`, `LastUpdatedBy`, `SessionId`, ModificationAction, DateAdded) VALUES (NEW.AliasSerNum, NEW.ExpressionName, NEW.Description, NEW.LastTransferred, NEW.LastUpdatedBy, NEW.SessionId, 'INSERT', NOW());
+END
