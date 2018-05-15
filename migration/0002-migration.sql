01372725590ff0ce0e5f3fe9e15d77ef340aceb7
Ackeem Joseph <ackeem.berry@gmail.com>
2018-05-15 15:41:24 -0400

    --- Removed migration files since we track from dbv

--- migration/0002-migration.sql
+++
@@ -1,7 +0,0 @@
-# Tracking report for table `Role`
-# 2017-07-17 12:44:03
-
-
-INSERT INTO `Role` (`RoleSerNum`, `RoleName`, `DateAdded`, `LastUpdated`) VALUES (NULL, 'clinician', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP), (NULL, 'manager', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-INSERT INTO `Role` (`RoleSerNum`, `RoleName`, `DateAdded`, `LastUpdated`) VALUES (NULL, 'education creator', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-UPDATE `Role` SET `RoleName` = 'education-creator' WHERE `Role`.`RoleSerNum` = 7;
--- migration/0006-migration.sql
+++
@@ -1,4 +0,0 @@
-# Tracking report for table `Questionnaire`
-# 2017-09-18 11:21:23
-RENAME TABLE Questionnaire TO QuestionnaireControlNew;
-
