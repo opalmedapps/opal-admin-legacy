c768fa9e50a9286f5a43fc46bab74dc4fb823fd2
Ackeem J <ackeem.berry@gmail.com>
2018-05-16 10:48:41 -0400

    --- Removed definer reference from triggers

--- dbv/data/schema/test_result_expression_update_trigger.sql
+++ dbv/data/schema/test_result_expression_update_trigger.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`ackeem`@`%` TRIGGER `test_result_expression_update_trigger` AFTER UPDATE ON `TestResultExpression`
+CREATE TRIGGER `test_result_expression_update_trigger` AFTER UPDATE ON `TestResultExpression`
  FOR EACH ROW BEGIN
 if NEW.LastPublished <=> OLD.LastPublished THEN
    INSERT INTO `TestResultExpressionMH`(`TestResultControlSerNum`,`ExpressionName`,`LastPublished`, `LastUpdatedBy`, `SessionId`, ModificationAction, DateAdded) VALUES (NEW.TestResultControlSerNum, NEW.ExpressionName, NEW.LastPublished, NEW.LastUpdatedBy, NEW.SessionId, 'UPDATE', NOW());
