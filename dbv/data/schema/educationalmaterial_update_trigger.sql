5a7b2becdf48c047388ebbfe57089fc177dee82a
Ackeem Joseph <ackeem.berry@gmail.com>
2018-05-16 10:48:41 -0400

    --- Removed definer reference from triggers

--- dbv/data/schema/educationalmaterial_update_trigger.sql
+++ dbv/data/schema/educationalmaterial_update_trigger.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`ackeem`@`%` TRIGGER `educationalmaterial_update_trigger` AFTER UPDATE ON `EducationalMaterial`
+CREATE TRIGGER `educationalmaterial_update_trigger` AFTER UPDATE ON `EducationalMaterial`
  FOR EACH ROW BEGIN
 INSERT INTO `EducationalMaterialMH`(`EducationalMaterialSerNum`, `CronLogSerNum`, `EducationalMaterialControlSerNum`, `PatientSerNum`, `DateAdded`, `ReadStatus`, `ModificationAction`) VALUES (NEW.EducationalMaterialSerNum, NEW.CronLogSerNum, NEW.EducationalMaterialControlSerNum, NEW.PatientSerNum, NOW(), NEW.ReadStatus, 'UPDATE');
 END
