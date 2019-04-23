366030c1703d174c6d8aabcd52d82cb817e65bac
Dominic Bourdua <dominic.bourdua@gmail.com>
2019-04-23 11:21:22 -0400

    Removed test code

--- php/questionnaire/delete.question.php
+++ php/questionnaire/delete.question.php
@@ -7,9 +7,6 @@ include_once('questionnaire.inc');
 $serNum = strip_tags($_POST['ID']);
 $userId = strip_tags($_POST['userId']);
 
-$userId = 20;
-$serNum = 885;
-
 // Call function
 $questionObj = new Question($userId); // Object
 $response = $questionObj->deleteQuestion($serNum);
