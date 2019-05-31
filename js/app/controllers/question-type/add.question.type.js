41cd670a5be42839c92cbd49d902840fbeec0440
Dominic Bourdua <dominic.bourdua@gmail.com>
2019-05-31 10:31:22 -0400

    Started creating the question type section

---
+++ js/app/controllers/question-type/add.question.type.js
@@ -0,0 +1,420 @@
+angular.module('opalAdmin.controllers.question.type.add', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui.grid', 'ui.bootstrap.materialPicker']).
+controller('question.add', function ($scope, $state, $filter, $uibModal, Session, filterCollectionService, questionnaireCollectionService) {
+	// navigation function
+	$scope.goBack = function () {
+		$state.go('questionnaire');
+	};
+
+	$scope.goBack = function () {
+		window.history.back();
+	};
+
+	// Default booleans
+	$scope.titleSection = { open: false, show: true };
+	$scope.answerTypeSection = { open: false, show: false };
+	$scope.questionLibrarySection = { open: false, show: false };
+
+	$scope.list = ["one", "two", "three", "four", "five", "six"];
+
+	// get current user id
+	var user = Session.retrieveObject('user');
+	var OAUserId = user.id;
+
+	// step bar
+	var steps = {
+		question: { completed: false },
+		answerType: { completed: false },
+		questionGroup: { completed: false }
+	};
+
+	$scope.numOfCompletedSteps = 0;
+	$scope.stepTotal = 2;
+	$scope.stepProgress = trackProgress($scope.numOfCompletedSteps, $scope.stepTotal);
+
+	$scope.decreaseStep = function() {
+		$scope.newQuestion.library_ID = null;
+		$scope.stepTotal = 2;
+		$scope.numOfCompletedSteps = 2;
+		$scope.stepProgress = trackProgress($scope.numOfCompletedSteps, $scope.stepTotal);
+		steps.question.completed = true;
+		$scope.questionLibrarySection.open = false;
+	};
+
+	$scope.increaseStep = function() {
+		$scope.stepTotal = 3;
+		steps.question.completed = false;
+		$scope.numOfCompletedSteps = 2;
+		$scope.stepProgress = trackProgress($scope.numOfCompletedSteps, $scope.stepTotal);
+		$scope.questionLibrarySection.open = true;
+	};
+
+	/* Function for the "Processing" dialog */
+	var processingModal;
+	$scope.showProcessingModal = function () {
+
+		processingModal = $uibModal.open({
+			templateUrl: 'templates/processingModal.html',
+			backdrop: 'static',
+			keyboard: false,
+		});
+	};
+
+	// Function to calculate / return step progress
+	function trackProgress(value, total) {
+		var result = Math.round(100 * value / total);
+		if (result > 100) result = 100;
+		return result;
+	}
+
+	// Function to return number of steps completed
+	function stepsCompleted(steps) {
+		var numberOfTrues = 0;
+		for (var step in steps) {
+			if (steps[step].completed === true) {
+				numberOfTrues++;
+			}
+		}
+		return numberOfTrues;
+	}
+
+	// Initialize the new question object
+	$scope.newQuestion = {
+		text_EN: "",
+		text_FR: "",
+		library_ID: null,
+		libraries: [],
+		typeId: null,
+		private: null,
+		OAUserId: OAUserId
+	};
+
+	// Initialize variables for holding selected answer type & group
+	$scope.selectedAt = null;
+	$scope.selectedGroup = null;
+
+	// Filter lists initialized
+	$scope.atFilterList = [];
+	$scope.libFilterList = [];
+	// $scope.catFilterList = [];
+	$scope.groupFilterList = [];
+	$scope.atCatList = [];
+
+	// Initialize search field variables
+	$scope.atEntered = '';
+	$scope.libEntered = '';
+	$scope.catEntered = '';
+	$scope.groupEntered = '';
+
+	// Update values from form
+	$scope.updateQuestionText = function () {
+
+		$scope.titleSection.open = true;
+		if (!$scope.newQuestion.text_EN && !$scope.newQuestion.text_FR) {
+			$scope.titleSection.open = false;
+		}
+		if ($scope.newQuestion.text_EN && $scope.newQuestion.text_FR) {
+
+			$scope.answerTypeSection.show = true;
+
+			steps.question.completed = true;
+			$scope.numOfCompletedSteps = stepsCompleted(steps);
+			$scope.stepProgress = trackProgress($scope.numOfCompletedSteps, $scope.stepTotal);
+
+		} else {
+
+			steps.question.completed = false;
+			$scope.numOfCompletedSteps = stepsCompleted(steps);
+			$scope.stepProgress = trackProgress($scope.numOfCompletedSteps, $scope.stepTotal);
+
+		}
+	};
+
+	$scope.updateAt = function (selectedAt) {
+
+		$scope.answerTypeSection.open = true;
+		if ($scope.newQuestion.typeId) {
+			$scope.questionLibrarySection.show = true;
+			$scope.selectedAt = selectedAt;
+			steps.answerType.completed = true;
+			$scope.numOfCompletedSteps = stepsCompleted(steps);
+
+			var increment = parseFloat($scope.selectedAt.increment);
+			var minValue = parseFloat($scope.selectedAt.minValue);
+			if (minValue === 0.0) minValue = increment;
+			var maxValue = parseFloat($scope.selectedAt.maxValue);
+
+			$scope.radiostep = new Array();
+			$scope.stepProgress = trackProgress($scope.numOfCompletedSteps, $scope.stepTotal);
+			for(var i = minValue; i <= maxValue; i += increment) {
+				$scope.radiostep.push({"name":""});
+			}
+			$scope.radiostep[0]["name"] = $scope.selectedAt.minCaption_EN + " / " + $scope.selectedAt.minCaption_FR;
+			$scope.radiostep[$scope.radiostep.length - 1]["name"] = $scope.selectedAt.maxCaption_EN + " / " + $scope.selectedAt.maxCaption_FR;
+		} else {
+
+			steps.answerType.completed = false;
+			$scope.numOfCompletedSteps = stepsCompleted(steps);
+			$scope.stepProgress = trackProgress($scope.numOfCompletedSteps, $scope.stepTotal);
+
+		}
+	};
+
+	$scope.updateLibrary = function (selectedLibrary) {
+		$scope.questionLibrarySection.open = true;
+
+		var idx = $scope.newQuestion.libraries.indexOf(selectedLibrary.serNum);
+
+		if (idx > -1) {
+			$scope.newQuestion.libraries.splice(idx, 1);
+		}
+
+		else {
+			$scope.newQuestion.libraries.push(selectedLibrary.serNum);
+		}
+
+		if ($scope.newQuestion.libraries.length > 0) {
+
+			$scope.selectedGroup = selectedLibrary;
+
+			steps.questionGroup.completed = true;
+			$scope.numOfCompletedSteps = 3;
+			$scope.stepProgress = trackProgress($scope.numOfCompletedSteps, $scope.stepTotal);
+
+		} else {
+
+			steps.questionGroup.completed = false;
+			$scope.numOfCompletedSteps = stepsCompleted(steps);
+			$scope.stepProgress = trackProgress($scope.numOfCompletedSteps, $scope.stepTotal);
+
+		}
+	};
+
+	// assign functions
+	$scope.searchAt = function (field) {
+		$scope.atEntered = field;
+	};
+	$scope.searchLib = function (field) {
+		$scope.libEntered = field;
+	};
+
+	// cancel selection
+	$scope.atCancelSelection = function () {
+		$scope.newQuestion.typeId = false;
+	};
+
+	$scope.groupCancelSelection = function () {
+		$scope.newQuestion.library_ID = false;
+	};
+
+	// search function
+	$scope.searchAtFilter = function (Filter) {
+		var keyword = new RegExp($scope.atEntered, 'i');
+		return !$scope.atEntered || keyword.test(Filter.name_EN);
+	};
+	$scope.searchLibFilter = function (Filter) {
+		var keyword = new RegExp($scope.libEntered, 'i');
+		return !$scope.libEntered || keyword.test(Filter.name_EN);
+	};
+
+	// questionnaire API: retrieve data
+	questionnaireCollectionService.getQuestionTypes(OAUserId).then(function (response) {
+		$scope.atFilterList = response.data;
+	}).catch(function(response) {
+		alert('Error occurred getting response types: '+response.status +"\r\n"+ response.data);
+	});
+
+	questionnaireCollectionService.getLibraries(OAUserId).then(function (response) {
+		$scope.groupFilterList = response.data;
+	}).catch(function(response) {
+		alert('Error occurred getting question libraries: '+response.status +"\r\n"+ response.data);
+	});
+
+	questionnaireCollectionService.getQuestionTypeList(OAUserId).then(function (response) {
+		$scope.atCatList = response.data;
+	}).catch(function(response) {
+		alert('Error occurred getting response type categories: '+response.status +"\r\n"+ response.data);
+	});
+
+	// add new types & write into DB
+	// Initialize the new answer type object
+	$scope.newAnswerType = {
+		name_EN: "",
+		name_FR: "",
+		category_EN: "",
+		category_FR: "",
+		private: 0,
+		OAUserId: OAUserId,
+		options: [],
+		slider: []
+	};
+
+	$scope.addNewQuestionType = function (atCatSelected) {
+		// Binding categories
+		$scope.newAnswerType.ID = atCatSelected.ID;
+		$scope.newAnswerType.OAUserId = Session.retrieveObject('user').id;
+		// Prompt to confirm user's action
+		var confirmation = confirm("Are you sure you want to create a new " + atCatSelected.category_EN.toLowerCase()  +  " response type named '" + $scope.newAnswerType.name_EN + "'?");
+		if (confirmation) {
+			// write in to db
+			$.ajax({
+				type: "POST",
+				url: "php/questionnaire/insert.question_type.php",
+				data: $scope.newAnswerType,
+				success: function (result) {
+					result = JSON.parse(result);
+					if(result.message === 200) {
+
+						alert('Successfully added the new response type. Please find your new response type in the form above.');
+						// update answer type list
+						questionnaireCollectionService.getQuestionTypes(OAUserId).then(function (response) {
+							$scope.atFilterList = response.data;
+						}).catch(function (response) {
+							alert('Error occurred getting response types: '+response.status +"\r\n"+ response.data);
+						});
+					} else {
+						alert("Unable to create the response type. Code " + result.message + ".\r\nError message: " + result.details);
+					}
+
+				},
+				error: function (request, status, err) {
+					alert("A problem occurred. Please try again.\r\n"+request.responseText);
+				}
+			});
+		}
+	};
+
+	// add options
+	$scope.addOptions = function () {
+		$scope.newAnswerType.options.push({
+			text_EN: "",
+			text_FR: "",
+			order: undefined,
+			OAUserId: OAUserId
+		});
+	};
+
+	// delete options
+	$scope.deleteOptions = function (optionToDelete) {
+		var index = $scope.newAnswerType.options.indexOf(optionToDelete);
+		if (index > -1) {
+			$scope.newAnswerType.options.splice(index, 1);
+		}
+	};
+
+	// Initialize the new library object
+	$scope.newLibrary = {
+		name_EN: "",
+		name_FR: "",
+		private: 0,
+		OAUserId: OAUserId
+	};
+	$scope.addNewLib = function () {
+		// Prompt to confirm user's action
+		var confirmation = confirm("Are you sure you want to create new library " + $scope.newLibrary.name_EN + " / "+$scope.newLibrary.name_FR+ "?");
+		if (confirmation) {
+			// write in to db
+			$.ajax({
+				type: "POST",
+				url: "php/questionnaire/insert.library.php",
+				data: $scope.newLibrary,
+				success: function (result) {
+					result = JSON.parse(result);
+					if(result.code === 200) {
+						alert('Successfully added the new library. Please find your new library in the panel above.');
+						questionnaireCollectionService.getLibraries(OAUserId).then(function (response) {
+							$scope.libraries = [];
+							$scope.groupFilterList = response.data;
+						}).catch(function (response) {
+							alert('Error occurred getting libraries. Code '+ response.status +"\r\n" + response.data);
+						});
+					}
+					else {
+						alert("Unable to create the library. Code " + result.code + ".\r\nError message: " + result.message);
+					}
+				},
+				error: function () {
+					alert("Something went wrong.");
+				}
+			});
+		}
+	};
+
+	// check if form is completed
+	$scope.checkForm = function () {
+		if (trackProgress($scope.numOfCompletedSteps, $scope.stepTotal) >= 100)
+			return true;
+		else
+			return false;
+	};
+
+	// submit question: write into DB
+	$scope.submitQuestion = function () {
+		if ($scope.checkForm()) {
+			// Submit
+			$.ajax({
+				type: "POST",
+				url: "php/questionnaire/insert.question.php",
+				data: $scope.newQuestion,
+				success: function (result) {
+					result = JSON.parse(result);
+					if (result.code === 200) {
+						$state.go('questionnaire-question');
+					} else {
+						alert("Unable to create the question. Code " + result.code + ".\r\nError message: " + result.message);
+					}
+				},
+				error: function () {
+					alert("Something went wrong.");
+				}
+			});
+		}
+	};
+
+	var fixmeTop = $('.summary-fix').offset().top;
+	$(window).scroll(function () {
+		var currentScroll = $(window).scrollTop();
+		if (currentScroll >= fixmeTop) {
+			$('.summary-fix').css({
+				position: 'fixed',
+				top: '0',
+				width: '15%'
+			});
+		} else {
+			$('.summary-fix').css({
+				position: 'static',
+				width: ''
+			});
+		}
+	});
+
+	var fixMeMobile = $('.mobile-side-panel-menu').offset().top;
+	$(window).scroll(function() {
+		var currentScroll = $(window).scrollTop();
+		if (currentScroll >= fixMeMobile) {
+			$('.mobile-side-panel-menu').css({
+				position: 'fixed',
+				top: '50px',
+				width: '100%',
+				zIndex: '100',
+				background: '#6f5499',
+				boxShadow: 'rgba(93, 93, 93, 0.6) 0px 3px 8px -3px'
+
+			});
+			$('.mobile-summary .summary-title').css({
+				color: 'white'
+			});
+		} else {
+			$('.mobile-side-panel-menu').css({
+				position: 'static',
+				width: '',
+				background: '',
+				boxShadow: ''
+			});
+			$('.mobile-summary .summary-title').css({
+				color: '#6f5499'
+			});
+		}
+	});
+
+
+});
---
+++ js/app/controllers/question-type/edit.question.type.js
@@ -0,0 +1,274 @@
+angular.module('opalAdmin.controllers.question.type.edit', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui.grid', 'ui.grid.expandable', 'ui.grid.resizeColumns'])
+
+	.controller('question.edit', function ($scope, $state, $filter, $uibModal, $uibModalInstance, questionnaireCollectionService, filterCollectionService, uiGridConstants, Session) {
+		// get current user id
+		var user = Session.retrieveObject('user');
+		var OAUserId = user.id;
+
+
+		// initialize default variables & lists
+		$scope.question = {};
+		$scope.libraries = [];
+		$scope.changesMade = false;
+		$scope.validSlider = true;
+
+
+		// Initialize variables for holding selected answer type & group
+		$scope.selectedLibrary = [];
+
+		$scope.setChangesMade = function () {
+			$scope.changesMade = true;
+		};
+
+		// Filter lists initialized
+		$scope.libraryFilterList = [];
+
+		// Initialize search field variables
+		$scope.libEntered = '';
+
+		// assign functions
+		$scope.searchLib = function (field) {
+			$scope.libEntered = field;
+		};
+
+		// search function
+		$scope.searchLibFilter = function (Filter) {
+			var keyword = new RegExp($scope.libEntered, 'i');
+			return !$scope.libEntered || keyword.test(Filter.name_EN);
+		};
+
+		$scope.orderPreview = function () {
+			$scope.question.subOptions.sort(function(a,b){
+				return (a.order > b.order) ? 1 : ((b.order > a.order) ? -1 : 0);
+			});
+		};
+
+		/*
+		$options["minValue"] <= 0.0 || $options["maxValue"] <= 0.0 || $options["increment"] <= 0.0 || $options["minValue"] >= $options["maxValue"])
+            HelpSetup::returnErrorMessage(HTTP_STATUS_INTERNAL_SERVER_ERROR, "Invalid data.");
+
+        $options["maxValue"] = floatval(floor(($options["maxValue"] - $options["minValue"]) / $options["increment"]) * $options["increment"]) + $options["minValue"];
+
+		* */
+
+
+		$scope.updateSlider = function () {
+			var radiostep = new Array();
+			var increment = parseFloat($scope.question.options.increment);
+			var minValue = parseFloat($scope.question.options.minValue);
+			var maxValue = parseFloat($scope.question.options.maxValue);
+
+			if (minValue <= 0.0 || maxValue <= 0.0 || increment <= 0 || minValue >= maxValue)
+				$scope.validSlider = false;
+			else {
+				maxValue = (Math.floor((maxValue - minValue) / increment) * increment) + minValue;
+				$scope.validSlider = true;
+				for(var i = minValue; i <= maxValue; i += increment) {
+					radiostep.push({"description":" " + i,"description_EN":" " + i,"description_FR":" " + i});
+				}
+				radiostep[0]["description_EN"] += " " + $scope.question.options.minCaption_EN;
+				radiostep[0]["description_FR"] += " " + $scope.question.options.minCaption_FR;
+				radiostep[radiostep.length - 1]["description_EN"] += " " + $scope.question.options.maxCaption_EN;
+				radiostep[radiostep.length - 1]["description_FR"] += " " + $scope.question.options.maxCaption_FR;
+			}
+			$scope.question.subOptions = radiostep;
+		}
+
+		/* Function for the "Processing" dialog */
+		var processingModal;
+		$scope.showProcessingModal = function () {
+
+			processingModal = $uibModal.open({
+				templateUrl: 'templates/processingModal.html',
+				backdrop: 'static',
+				keyboard: false,
+			});
+		};
+
+		$scope.checkForm = function () {
+			if ($scope.question.text_EN && $scope.question.text_FR && $scope.changesMade) {
+				if($scope.question.typeId === "2") {
+					if ($scope.question.options.increment <= 0 || $scope.question.options.minValue <= 0 || $scope.question.options.maxValue <= 0 || $scope.question.options.minValue > $scope.question.options.maxValue || $scope.question.options.minCaption_EN === "" || $scope.question.options.minCaption_FR === "" || $scope.question.options.maxCaption_EN === "" || $scope.question.options.maxCaption_FR === "" )
+						return false;
+					else
+						return true;
+				} else if ($scope.question.typeId === "4" || $scope.question.typeId === "1") {
+					var loopResult = true;
+					$scope.question.subOptions.forEach(function(entry) {
+						if (entry.description_EN ==="" || entry.description_FR ==="" || entry.order === "")
+							loopResult = false;
+					});
+					return loopResult;
+				}
+				else
+					return true;
+			}
+			else
+				return false;
+		};
+
+		// Show processing dialog on load
+		$scope.showProcessingModal();
+
+		// Call our API service to get the questionnaire details
+		questionnaireCollectionService.getQuestionDetails($scope.currentQuestion.serNum, OAUserId).then(function (response) {
+			questionnaireCollectionService.getLibraries(OAUserId).then(function (resp) {
+				$scope.libraryFilterList = resp.data;
+			}).catch(function (response) {
+				alert('Error occurred getting libraries. Code '+ response.status +"\r\n" + response.data);
+			});
+
+			// Assign value
+			$scope.question = response.data;
+
+			if($scope.question.typeId === "2") {
+				$scope.question.options.minValue = parseInt($scope.question.options.minValue);
+				$scope.question.options.maxValue = parseInt($scope.question.options.maxValue);
+				$scope.question.options.increment = parseInt($scope.question.options.increment);
+				$scope.updateSlider();
+			}
+
+			if($scope.question.subOptions !== null) {
+				$scope.question.subOptions.forEach(function(entry) {
+					entry.order = parseInt(entry.order);
+				});
+			}
+
+			if (response.data.private === "1")
+				$scope.question.private = true;
+			else
+				$scope.question.private = false;
+			if (response.data.final === "1")
+				$scope.question.final = true;
+			else
+				$scope.question.final = false;
+			if (response.data.readOnly === "1")
+				$scope.question.readOnly = true;
+			else
+				$scope.question.readOnly = false;
+			if (response.data.isOwner === "1")
+				$scope.question.isOwner = true;
+			else
+				$scope.question.isOwner = false;
+
+			$scope.question.OAUserId = OAUserId;
+
+			$scope.selectedLibrary = response.data.libraries;
+
+			processingModal.close(); // hide modal
+			processingModal = null; // remove reference
+		}).catch(function (err) {
+			alert('Error occurred getting question details.\r\nCode ' + err.status + " " + err.data);
+			processingModal.close(); // hide modal
+			processingModal = null; // remove reference
+		});
+
+		// Call our API service to get the list of existing answer types
+
+		questionnaireCollectionService.getQuestionTypes(OAUserId).then(function (response) {
+			$scope.atFilterList = response.data;
+		}).catch(function (response){
+			alert('Error occurred getting response types:', response.status, response.data);
+		});
+
+		// Function to close modal dialog
+		$scope.cancel = function () {
+			$uibModalInstance.dismiss('cancel');
+		};
+
+		$scope.updateLibrary = function (selectedLibrary) {
+			$scope.changesMade = true;
+			var idx = $scope.selectedLibrary.indexOf(selectedLibrary.serNum);
+			if (idx > -1)
+				$scope.selectedLibrary.splice(idx, 1);
+			else
+				$scope.selectedLibrary.push(selectedLibrary.serNum);
+
+		};
+
+		$scope.addOptions = function () {
+			$scope.question.subOptions.push({
+				description_EN: "",
+				description_FR: "",
+				order: $scope.question.subOptions.length+1,
+				OAUserId: OAUserId
+			});
+		};
+
+		// delete options
+		$scope.deleteOptions = function (optionToDelete) {
+			var index = $scope.question.subOptions.indexOf(optionToDelete);
+			if (index > -1) {
+				$scope.question.subOptions.splice(index, 1);
+				$scope.changesMade = true;
+			}
+		};
+
+		questionnaireCollectionService.getLibraries(OAUserId).then(function (response) {
+			$scope.groupFilterList = response.data;
+		}).catch(function(response) {
+			alert('Error occurred getting question libraries:', response.status, response.data);
+		});
+
+		$scope.newLibrary = {
+			name_EN: "",
+			name_FR: "",
+			private: 0,
+			OAUserId: OAUserId
+		};
+
+		$scope.addNewLib = function () {
+			// Prompt to confirm user's action
+			var confirmation = confirm("Are you sure you want to create new library " + $scope.newLibrary.name_EN + " / "+$scope.newLibrary.name_FR+ "?");
+			if (confirmation) {
+				// write in to db
+				$.ajax({
+					type: "POST",
+					url: "php/questionnaire/insert.library.php",
+					data: $scope.newLibrary,
+					success: function (result) {
+						result = JSON.parse(result);
+						if(result.code === 200) {
+							alert('Successfully added the new library. Please find your new library in the panel above.');
+
+							questionnaireCollectionService.getLibraries(OAUserId).then(function (response) {
+								$scope.libraries = [];
+								$scope.libraryFilterList = response.data;
+							}).catch(function (response) {
+								alert('Error occurred getting libraries. Code '+ response.status +"\r\n" + response.data);
+							});
+						}
+						else {
+							alert("Unable to create the library. Code " + result.code + ".\r\nError message: " + result.message);
+						}
+					},
+					error: function () {
+						alert("Something went wrong.");
+					}
+				});
+			}
+		};
+
+		// Submit changes
+		$scope.updateQuestion = function () {
+			// Submit form
+			$.ajax({
+				type: "POST",
+				url: "php/questionnaire/update.question.php",
+				data: $scope.question,
+				success: function (response) {
+					response = JSON.parse(response);
+
+					// Show success or failure depending on response
+					if (response.code === 200) {
+						$scope.setBannerClass('success');
+						$scope.$parent.bannerMessage = "Successfully updated \"" + $scope.question.text_EN.replace(/<(?:.|\n)*?>/gm, '') + " / " + $scope.question.text_FR.replace(/<(?:.|\n)*?>/gm, '') + "\"!";
+						$uibModalInstance.close();
+						$scope.showBanner();
+					}
+					else
+						alert("An error occurred, code "+response.code+". Please review the error message below.\r\n" + response.message);
+				}
+			});
+		};
+	});
