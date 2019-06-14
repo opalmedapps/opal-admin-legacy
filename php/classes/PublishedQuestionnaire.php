a5bc89c0a55cc231ed1ab008cc0d3b4de976bb13
Dominic Bourdua <dominic.bourdua@gmail.com>
2019-06-14 11:12:22 -0400

    Modified the fields creationDate from TimeStamp to DateTime because our servers dont support multiple triggers in a same table.
    Added creationDate data manually and processing of exception in it.

--- php/classes/PublishedQuestionnaire.php
+++ php/classes/PublishedQuestionnaire.php
@@ -7,6 +7,12 @@
 
 class PublishedQuestionnaire extends Questionnaire {
 
+    public function __construct($OAUserId = false, $sessionId = false) {
+        parent::__construct($OAUserId);
+
+        $this->opalDB->setSessionId($sessionId);
+    }
+
     /*
      * This function returns all the published questionnaires from the Questionnaire Control
      * @params  void
@@ -29,9 +35,42 @@ class PublishedQuestionnaire extends Questionnaire {
             $titles = $this->questionnaireDB->getQuestionnaireNames($row["db_serial"]);
             $row["expression_EN"] = $titles["title_EN"];
             $row["expression_FR"] = $titles["title_FR"];
-            $row["triggers"] = $this->opalDB->getFilters($row["QuestionnaireControlSerNum"]);
+            $row["triggers"] = $this->opalDB->getFilters($row["serial"]);
             $row["occurrence"] = $occurrenceArray;
         }
         return $publishedQuestionnaires;
+    }
+
+    /*
+     * Validate and saniteze the list of published flags for questionnaire
+     * @params  array of questionnaire to mark as published or not ($_POST)
+     * @return  array of sanitize data
+     * */
+
+    function validateAndSanitizePublicationList($toValidate) {
+        $validatedList = array();
+        foreach($toValidate as $questionnaire) {
+            $id = trim(strip_tags($questionnaire["serial"]));
+            $publish = intval(trim(strip_tags($questionnaire["publish"])));
+            if ($publish != 0 || $publish != 1)
+                $publish = 0;
+            array_push($validatedList, array("serial"=>$id, "publish"=>$publish));
+        }
+        return $validatedList;
+    }
+
+    /*
+     * Update the status of a series of questionnaires (published = 1 / unpublished = 0)
+     * @params  array of ID with the publication flag
+     *          for example:    array(
+     *                              array("serial"=>1, "publish"=>0),
+     *                              array("serial"=>2, "publish"=>1),
+     *                              array("serial"=>3, "publish"=>0),
+     *                              ...
+     *                          )
+     * @return  void
+     * */
+    function updatePublicationFlags($list) {
+
     }
 }
