04720e7a70cf78aeffc27f49b3ecbc3fa7be6fad
Dominic Bourdua <dominic.bourdua@gmail.com>
2019-08-14 08:24:35 -0400

    Fixing a bug with htaccess file

--- php/post/update.post_publish_flags.php
+++ php/post/update.post_publish_flags.php
@@ -1,15 +1,14 @@
 <?php
-	header('Content-Type: application/javascript');
-	/* To call Post Object to update post when the "Publish Flag" checkbox has been changed */
-	include_once('post.inc');
+/* To call Post Object to update post when the "Publish Flag" checkbox has been changed */
+include_once('post.inc');
 
-	$postObject = new Post; // Object
+$postObject = new Post; // Object
 
-	// Retrieve FORM params
-	$postFlags	= $_POST['flagList'];
-	$user 			= $_POST['user'];
+// Retrieve FORM params
+$postFlags	= $_POST['flagList'];
+$user 			= $_POST['user'];
 
-	// Call function
-  $response = $postObject->updatePostFlags($postFlags, $user);
-  print json_encode($response); // Return response
-?>
+// Call function
+$response = $postObject->updatePostFlags($postFlags, $user);
+header('Content-Type: application/javascript');
+echo json_encode($response); // Return response
