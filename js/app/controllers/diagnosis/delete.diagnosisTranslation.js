2624cb9ad75e81a8804cc678a36bde234a977acf
Dominic Bourdua <dominic.bourdua@muhc.mcgill.ca>
2019-09-18 10:40:33 -0400

    Translated diagnosis translation listing and delete page

--- js/app/controllers/diagnosis/delete.diagnosisTranslation.js
+++ js/app/controllers/diagnosis/delete.diagnosisTranslation.js
@@ -1,37 +1,40 @@
 angular.module('opalAdmin.controllers.diagnosisTranslation.delete', ['ngAnimate', 'ui.bootstrap', 'ui.grid', 'ui.grid.resizeColumns']).
 
-	controller('diagnosisTranslation.delete', function ($scope, $filter, $uibModal, $uibModalInstance, diagnosisCollectionService, educationalMaterialCollectionService, uiGridConstants, $state, Session) {
+controller('diagnosisTranslation.delete', function ($scope, $filter, $uibModal, $uibModalInstance, diagnosisCollectionService, educationalMaterialCollectionService, uiGridConstants, $state, Session) {
 
-		// Submit delete
-		$scope.deleteDiagnosisTranslation = function () {
-			// Log who deleted diagnosis translation
-			var currentUser = Session.retrieveObject('user');
-			$scope.diagnosisTranslationToDelete.user = currentUser;
-			$.ajax({
-				type: "POST",
-				url: "diagnosis-translation/delete/diagnosis-translation",
-				data: $scope.diagnosisTranslationToDelete,
-				success: function (response) {
-					response = JSON.parse(response);
-					// Show success or failure depending on response
-					if (response.value) {
-						$scope.setBannerClass('success');
-						$scope.$parent.bannerMessage = "Successfully deleted \"" + $scope.diagnosisTranslationToDelete.name_EN + "/ " + $scope.diagnosisTranslationToDelete.name_FR + "\"!";
-					}
-					else {
-						$scope.setBannerClass('danger');
-						$scope.$parent.bannerMessage = response.message;
-					}
+	// Submit delete
+	$scope.deleteDiagnosisTranslation = function () {
+		// Log who deleted diagnosis translation
+		var currentUser = Session.retrieveObject('user');
+		$scope.diagnosisTranslationToDelete.user = currentUser;
+		$.ajax({
+			type: "POST",
+			url: "diagnosis-translation/delete/diagnosis-translation",
+			data: $scope.diagnosisTranslationToDelete,
+			success: function (response) {
+				response = JSON.parse(response);
+				// Show success or failure depending on response
+				if (response.value) {
+					$scope.setBannerClass('success');
+					$scope.$parent.bannerMessage = $filter('translate')('DIAGNOSIS.DELETE.SUCCESS');
 					$scope.showBanner();
-					$uibModalInstance.close();
 				}
-			});
-		};
+				else {
+					alert($filter('translate')('DIAGNOSIS.DELETE.ERROR') + "\r\n\r\n" + response.message);
+				}
+				$uibModalInstance.close();
+			},
+			error: function (err) {
+				alert($filter('translate')('DIAGNOSIS.DELETE.ERROR') + "\r\n\r\n" + err.status + " - " + err.statusText);
+				$uibModalInstance.close();
+			}
+		});
+	};
 
-		// Function to close modal dialog
-		$scope.cancel = function () {
-			$uibModalInstance.dismiss('cancel');
-		};
+	// Function to close modal dialog
+	$scope.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
 
 
 });
