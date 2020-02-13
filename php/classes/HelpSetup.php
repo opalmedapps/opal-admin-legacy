77049090ab95f3969be644725d89d1534add52c1
Dominic Bourdua <dominic.bourdua@gmail.com>
2020-02-13 09:22:01 -0500

    opal-109 - updated error msg function to be compatible with php 5.3

--- php/classes/HelpSetup.php
+++ php/classes/HelpSetup.php
@@ -11,8 +11,23 @@ class HelpSetup {
      * Basic functions to return an error message to the caller
      * */
     public static function returnErrorMessage($errcode = HTTP_STATUS_INTERNAL_SERVER_ERROR, $details) {
-        http_response_code($errcode);
+        if (!function_exists('http_response_code'))
+        {
+            function http_response_code($newcode = NULL)
+            {
+                static $code = 200;
+                if($newcode !== NULL)
+                {
+                    header('X-PHP-Response-Code: '.$newcode, true, $newcode);
+                    if(!headers_sent())
+                        $code = $newcode;
+                }
+                return $code;
+            }
+        }
+
         header('Content-Type: application/javascript');
+        http_response_code($errcode);
         echo json_encode($details);
         die();
     }
