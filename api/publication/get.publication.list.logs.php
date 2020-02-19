d063e7a1e372009bd307c98f6f74b0aa10694d6e
Dominic Bourdua <dominic.bourdua@gmail.com>
2020-02-19 08:39:45 -0500

    OPAL-179 - finished chart logs and list logs.

--- api/publication/get.publication.list.logs.php
+++ api/publication/get.publication.list.logs.php
@@ -2,12 +2,13 @@
 
 include_once('publication.inc');
 
+$cronIds = json_decode(strip_tags($_POST['cronIds']), true);
 $publicationId = strip_tags($_POST['publicationId']);
 $moduleId = strip_tags($_POST['moduleId']);
 $OAUserId = strip_tags($_POST['OAUserId']);
 
 $publication = new Publication($OAUserId);
-$result = $publication->getPublicationChartLogs($moduleId, $publicationId);
+$result = $publication->getPublicationListLogs($moduleId, $publicationId, $cronIds);
 
 header('Content-Type: application/javascript');
 echo json_encode($result);
