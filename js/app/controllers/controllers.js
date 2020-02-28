c23ccd7b525c404f065364a5edc00cb7fe117c01
Dominic Bourdua <dominic.bourdua@muhc.mcgill.ca>
2020-02-28 14:28:32 -0500

    OPAL-218 - Preparing files for custom codes

--- js/app/controllers/controllers.js
+++ js/app/controllers/controllers.js
@@ -56,9 +56,6 @@ angular.module('opalAdmin.controllers', [
 	'opalAdmin.controllers.questionnaire.add',
 	'opalAdmin.controllers.questionnaire.edit',
 	'opalAdmin.controllers.questionnaire.delete',
-	// 'opalAdmin.controllers.publication.tool',
-	// 'opalAdmin.controllers.publication.tool.add',
-	// 'opalAdmin.controllers.publication.tool.edit',
 	'opalAdmin.controllers.publication',
 	'opalAdmin.controllers.publication.add',
 	'opalAdmin.controllers.publication.edit',
@@ -74,7 +71,11 @@ angular.module('opalAdmin.controllers', [
 	'opalAdmin.controllers.diagnosisTranslation',
 	'opalAdmin.controllers.diagnosisTranslation.add',
 	'opalAdmin.controllers.diagnosisTranslation.edit',
-	'opalAdmin.controllers.diagnosisTranslation.delete'
+	'opalAdmin.controllers.diagnosisTranslation.delete',
+	'opalAdmin.controllers.customCodes',
+	'opalAdmin.controllers.customCodes.add',
+	'opalAdmin.controllers.customCodes.edit',
+	'opalAdmin.controllers.customCodes.delete'
 ]);
 
 
---
+++ js/app/controllers/custom-codes/delete.custom.code.js
@@ -0,0 +1,40 @@
+angular.module('opalAdmin.controllers.customCodes.delete', ['ngAnimate', 'ui.bootstrap', 'ui.grid', 'ui.grid.resizeColumns']).
+
+controller('customCodes.delete', function ($scope, $filter, $uibModal, $uibModalInstance, diagnosisCollectionService, educationalMaterialCollectionService, uiGridConstants, $state, Session) {
+
+	// Submit delete
+	$scope.deleteDiagnosisTranslation = function () {
+		// Log who deleted diagnosis translation
+		var currentUser = Session.retrieveObject('user');
+		$scope.diagnosisTranslationToDelete.user = currentUser;
+		$.ajax({
+			type: "POST",
+			url: "diagnosis-translation/delete/diagnosis-translation",
+			data: $scope.diagnosisTranslationToDelete,
+			success: function (response) {
+				response = JSON.parse(response);
+				// Show success or failure depending on response
+				if (response.value) {
+					$scope.setBannerClass('success');
+					$scope.$parent.bannerMessage = $filter('translate')('DIAGNOSIS.DELETE.SUCCESS');
+					$scope.showBanner();
+				}
+				else {
+					alert($filter('translate')('DIAGNOSIS.DELETE.ERROR') + "\r\n\r\n" + response.message);
+				}
+				$uibModalInstance.close();
+			},
+			error: function (err) {
+				alert($filter('translate')('DIAGNOSIS.DELETE.ERROR') + "\r\n\r\n" + err.status + " - " + err.statusText);
+				$uibModalInstance.close();
+			}
+		});
+	};
+
+	// Function to close modal dialog
+	$scope.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
+
+
+});
