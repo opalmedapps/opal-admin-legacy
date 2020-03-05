2c95f1d27693f9dc866e12cbfc1e10df7a3d379c
Dominic Bourdua <dominic.bourdua@muhc.mcgill.ca>
2020-03-05 12:50:55 -0500

    Fixed adding custom code by adding the type.

--- php/classes/CustomCode.php
+++ php/classes/CustomCode.php
@@ -21,21 +21,47 @@ class CustomCode extends OpalProject {
     }
 
     public function insertCustomCode($customCode) {
+
+
         $customCode = $this->arraySanitization($customCode);
         $moduleDetails = $this->opalDB->getModuleSettings($customCode["moduleId"]["value"]);
 
-        $this->_validateCustomCode($customCode);
+        $result = $this->_validateCustomCode($customCode, $moduleDetails);
+        if(count($result) > 0)
+            HelpSetup::returnErrorMessage(HTTP_STATUS_INTERNAL_SERVER_ERROR, "Custom code validation failed. " . implode(" ", $result));
+
         $toInsert = array(
             "code"=>strip_tags($customCode["details"]["code"]),
             "description"=>strip_tags($customCode["details"]["description"]),
             "source"=>LOCAL_SOURCE_ONLY,
         );
+        if(array_key_exists("type", $customCode) && array_key_exists("ID", $customCode["type"]))
+            $toInsert["type"] = $customCode["type"]["ID"];
 
         return $this->opalDB->insertCustomCode($toInsert, $moduleDetails["ID"]);
     }
 
-    protected function _validateCustomCode(&$customCode) {
-        if($customCode["details"]["code"] == "" || $customCode["details"]["description"] == "" || $customCode["moduleId"]["value"] == "")
-            HelpSetup::returnErrorMessage(HTTP_STATUS_INTERNAL_SERVER_ERROR, "Invalid custom code.");
+    protected function _validateCustomCode(&$customCode, &$moduleDetails) {
+        $errMsgs = array();                                             //By default, no error message
+        $typeFound = false;
+
+        if($customCode["details"]["code"] == "" || $customCode["details"]["description"] == "" || $customCode["moduleId"]["value"] == "") {
+            array_push($errMsgs, "Missing custom code info.");
+        }
+
+        $moduleDetails["subModule"] = json_decode($moduleDetails["subModule"], true);
+        if(count($moduleDetails["subModule"]) > 0) {
+            foreach ($moduleDetails["subModule"] as $item) {
+                if($item["ID"] == $customCode["type"]["ID"])
+                    $typeFound = true;
+            }
+            if(!$typeFound)
+                array_push($errMsgs, "Missing type.");
+        }
+        else {
+            if(array_key_exists("type", $customCode) && array_key_exists("ID", $customCode["type"]))
+                array_push($errMsgs, "Type should not be there.");
+        }
+        return $errMsgs;
     }
 }
