3b507ec7bd4f1a4813e81f28fbee43592b20992e
Dominic Bourdua <dominic.bourdua@gmail.com>
2020-03-06 12:51:40 -0500

    OPAL-206 Finished coding deletion of custom code.

---
+++ php/custom-code/delete.custom.code.php
@@ -0,0 +1,14 @@
+<?php
+
+/* To delete a custom code */
+include_once('custom.code.inc');
+
+$customCodeId = strip_tags($_POST['customCodeId']);
+$moduleId = strip_tags($_POST['moduleId']);
+$OAUserId = strip_tags($_POST['OAUserId']);
+
+$customCode = new CustomCode($OAUserId); // Object
+$response = $customCode->deleteCustomCode($customCodeId, $moduleId);
+
+header('Content-Type: application/javascript');
+echo json_encode($response); // Return response
