513600ad5c26182a9ab70290616a77e5b345061b
Karan Sharma <SHKA6013@MUHCAD.MUHCFRD.CA>
2020-04-06 16:32:26 -0400

    first commit for audit logs

---
+++ js/app/controllers/reports/auditReport.js
@@ -0,0 +1,12 @@
+angular.module('opalAdmin.controllers.auditReport', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ui.grid', 'ui.grid.grouping',
+	'ui.grid.autoResize',
+	'ui.grid.exporter',
+	'ui.grid.pagination',
+	'ui.grid.resizeColumns',
+	'ui.grid.moveColumns',
+	'ui.grid.pinning', 'textAngular'])
+	// eslint-disable-next-line no-unused-vars
+	.controller('auditReport',['$sce', '$scope', '$state', '$filter', '$timeout', '$uibModal', 'filterCollectionService', 'Session', 'uiGridConstants', 'uiGridGroupingConstants', function ($sce, $scope, $state, $filter, $timeout, $uibModal, filterCollectionService, Session, uiGridConstants,uiGridGroupingConstants)  {
+
+		}
+	]);
