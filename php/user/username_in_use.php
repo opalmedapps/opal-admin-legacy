a56199f323073347af40cfa31277b4b9578110ef
Dominic Bourdua <dominic.bourdua@gmail.com>
2020-04-14 11:47:11 -0400

    OPAL-266 - refactoring of User underway

--- php/user/username_in_use.php
+++ php/user/username_in_use.php
@@ -2,10 +2,9 @@
 
 include_once('user.inc');
 
-
 $username = strip_tags($_POST['username']);
 $userObj = new User(); // Object
-$Response = $userObj->usernameAlreadyInUse($username);
+$count = $userObj->usernameAlreadyInUse($username);
 
 header('Content-Type: application/javascript');
-echo json_encode($Response);
+echo json_encode(array("count"=>$count));
