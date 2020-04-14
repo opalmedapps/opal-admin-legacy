4e8ba49443d3d29b348aef7e1881440bef04030a
Karan Sharma <SHKA6013@MUHCAD.MUHCFRD.CA>
2020-04-14 11:36:05 -0400

    OPAL-267: Changing syntax

--- api/patient/find_patient.php
+++ api/patient/find_patient.php
@@ -5,7 +5,7 @@ include_once('patient.inc');
 
 $ssn = strip_tags($_POST['ssn']);
 $id = strip_tags($_POST['id']);
-$patientObj = new Patient(); // Object
+$patientObj = new Patient; // Object
 $patientResponse = $patientObj->findPatient($ssn, $id);
 
 //INSERT INTO AUDIT LOGS TABLE
