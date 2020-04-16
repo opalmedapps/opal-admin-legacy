db745550e8f4911176ea4e91b2d1a7ebcf8da9c9
Dominic Bourdua <dominic.bourdua@muhc.mcgill.ca>
2020-04-16 14:38:47 -0400

    OPAL-266 - OPAL-290 - End of coding the AD system

--- php/user/username_in_use.php
+++ php/user/username_in_use.php
@@ -4,7 +4,7 @@ include_once('user.inc');
 
 $username = strip_tags($_POST['username']);
 $userObj = new User(); // Object
-$count = $userObj->usernameAlreadyInUse($username);
+$count = $userObj->usernameExists($username);
 
 header('Content-Type: application/javascript');
 echo json_encode(array("count"=>$count));
