639bdbb5c5e2765a243f26cf1a2fa9c3d31ea2cc
Dominic Bourdua <dominic.bourdua@gmail.com>
2020-04-28 15:24:16 -0400

    OPAL-276 - Fixed minor bugs and finished list view of studies

--- php/study/get.studies.php
+++ php/study/get.studies.php
@@ -4,8 +4,8 @@ include_once('study.inc');
 
 $OAUserId = strip_tags($_POST["OAUserId"]);
 
-$customCode = new Study($OAUserId); // Object
-$results = $customCode->getStudies();
+$study = new Study($OAUserId); // Object
+$results = $study->getStudies();
 
 header('Content-Type: application/javascript');
 echo json_encode($results);
