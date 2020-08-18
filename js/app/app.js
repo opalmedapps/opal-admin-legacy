e97c822159e5e81155ce15203c413b80f17ff9c8
Dominic Bourdua <dominic.bourdua@gmail.com>
2020-08-18 14:44:37 -0400

    opal-530 - finished audit view list

--- js/app/app.js
+++ js/app/app.js
@@ -155,6 +155,7 @@ angular.module('opalAdmin', [
 			.state('role-add', { url: '/role/add', templateUrl: "templates/role/add.role.html", controller: "role.add", data: { requireLogin: true } })
 			.state('alert', { url: '/alert', templateUrl: "templates/alert/alerts.html", controller: "alert", data: { requireLogin: true } })
 			.state('alert-add', { url: '/alert/add', templateUrl: "templates/alert/add.alert.html", controller: "alert.add", data: { requireLogin: true } })
+			.state('audit', { url: '/audit', templateUrl: "templates/audit/audits.html", controller: "audit", data: { requireLogin: true } })
 			.state('user-activity', { url: '/user-activity', templateUrl: "templates/user/user-activity.html", controller: "userActivity", data: { requireLogin: true } })
 			.state('protected-route', { url: '/protected', resolve: { auth: function resolveAuthentication(AuthResolver) { return AuthResolver.resolve(); } } });
 
