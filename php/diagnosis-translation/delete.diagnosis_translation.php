f4e9a7682207f29efdda630dce8a02b9e812a160
Dominic Bourdua <dominic.bourdua@gmail.com>
2020-08-28 09:43:06 -0400

    opal-559 - finished refactor Diagnosis.php

--- php/diagnosis-translation/delete.diagnosis_translation.php
+++ php/diagnosis-translation/delete.diagnosis_translation.php
@@ -1,11 +1,8 @@
 <?php
 include_once("../config.php");
 
-$serial = $_POST['serial'];
-$user = $_POST['user'];
-
 $diagnosis = new Diagnosis();
-$response = $diagnosis->deleteDiagnosisTranslation($serial, $user);
+$response = $diagnosis->deleteDiagnosisTranslation($_POST);
 
 header('Content-Type: application/javascript');
 http_response_code(HTTP_STATUS_SUCCESS);
