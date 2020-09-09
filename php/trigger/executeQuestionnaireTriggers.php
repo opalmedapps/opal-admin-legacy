4db56e204588a94996ef9987647591accc55e50a
Ackeem <ackeem.berry@gmail.com>
2020-09-09 12:24:56 -0400

    Testing system login

--- php/trigger/executeQuestionnaireTriggers.php
+++ php/trigger/executeQuestionnaireTriggers.php
@@ -1,6 +1,46 @@
 <?php
 
 include_once("../config.php");
+            
+$cypher = null;
+while (isNull($cypher)) {
+    $cypher = time() % floor( rand() * 20 ) + 103;
+}
+$creds->Username = "TriggerSystem";
+$creds->Password = "pcGNdtwTV8Pd79FkLhP!ejH8Y^KR&4@u";
+$toEncrypt =  strval(json_encode($creds));
+
+$encrypted = Encrypt::encodeStringSystem($toEncrypt, $cypher);
+$loginUrl = "https://lxkvmap97/opalAdmin/user/system-login";
+
+# Our new data
+$postData = array(
+    'encrypted' => $encrypted,
+    'cypher' => $cypher
+);
+# Form data string
+$postString = http_build_query($postData, '', '&');
+
+# You can also check..
+if(function_exists('http_post_data') == false) {
+    $ch = curl_init($loginUrl);
+  
+    # Setting our options
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    # Get the response
+    $response = curl_exec($ch);
+    echo "RESPONSE CURL: $response";
+    curl_close($ch);
+}
+
+else {
+    $response = http_post_data($url, $postString);
+    echo "RESPONSE OTHER: $response";
+}
+
+die();
 
 $trigger = new Trigger(true); // guest status on for now
 $triggerType = MODULE_QUESTIONNAIRE; // define what type of trigger this is
