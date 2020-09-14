bd404808e13be461da66435841f6bb84ca49127f
Ackeem <ackeem.berry@gmail.com>
2020-09-14 15:53:50 -0400

    Initiate testing after removal of encrypt tool

--- php/trigger/executeQuestionnaireTriggers.php
+++ php/trigger/executeQuestionnaireTriggers.php
@@ -2,39 +2,6 @@
 
 include_once("../config.php");
    
-/* --- BEGIN SECTION TO CONNECT TO OPALADMIN --- */
-/* --- TODO: THIS NEEDS TO BE IN A METHOD --- */
-$cypher = null;
-while (is_null($cypher)) {
-    $cypher = time() % floor( rand() * 20 ) + 103;
-}
-$creds = array("username" => "TriggerSystem", "password" => "pcGNdtwTV8Pd79FkLhP!ejH8Y^KR&4@u");
-$toEncrypt =  strval(json_encode($creds));
-
-$encrypted = Encrypt::encodeStringSystem($toEncrypt, $cypher);
-$loginUrl = "https://lxkvmap97/opalAdmin/user/system-login";
-
-# Our new data
-$postData = array(
-    'encrypted' => $encrypted,
-    'cypher' => $cypher
-);
-# Form data string
-$postString = http_build_query($postData, '', '&');
-
-$ch = curl_init($loginUrl);
-
-# Setting our options
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-# Get the response
-$response = curl_exec($ch);
-echo "RESPONSE CURL: $response";
-curl_close($ch);
-
-/* --- END SECTION TO CONNECT TO OPALADMIN --- */
-
 $trigger = new Trigger();
 $sourceModuleId = MODULE_QUESTIONNAIRE; // define what type of trigger this is
 
