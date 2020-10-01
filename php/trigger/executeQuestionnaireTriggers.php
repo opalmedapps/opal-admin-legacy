8de5b303d94c23416a92eaf10ca5cd2ff9119933
Ackeem <ackeem.berry@gmail.com>
2020-10-01 09:03:51 -0400

    --- Switched api call to use incoming POST

--- php/trigger/executeQuestionnaireTriggers.php
+++ php/trigger/executeQuestionnaireTriggers.php
@@ -6,8 +6,8 @@ $trigger = new Trigger(true);
 $sourceModuleId = MODULE_QUESTIONNAIRE; // define what type of trigger this is
 
 // Need patientQuestionnaireSerNum from caller
-//$result = $trigger->executeTrigger($_POST, $sourceModuleId);
-$result = $trigger->executeTrigger(array("id" => 200), $sourceModuleId);
+$result = $trigger->executeTrigger($_POST, $sourceModuleId);
+//$result = $trigger->executeTrigger(array("id" => 200), $sourceModuleId);
 
 header('Content-Type: application/javascript');
 echo json_encode($result); // Return response
