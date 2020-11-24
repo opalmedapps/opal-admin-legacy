ac80a043c83c51ba7ff817cfeec9ec1d2d048a07
Dominic Bourdua <dominic.bourdua@muhc.mcgill.ca>
2020-11-24 10:37:04 -0500

    OPAL-609 - adding API calls for source tasks, documents and appointments

--- php/master-source/is.source.appointment.exists.php
+++ php/master-source/is.source.appointment.exists.php
@@ -1,8 +1,9 @@
 <?php
+
 include_once("../config.php");
 
-$sourceDiag = new MasterSourceAlias(); // Object
-$results = $sourceDiag->getSourceAliasDetails($_POST);
+$sourceDiag = new MasterSourceAppointment(); // Object
+$results = $sourceDiag->doesAppointmentExists($_POST);
 
 header('Content-Type: application/javascript');
 echo json_encode($results);
