247aafc143fad5f7e385baa5c96a5e2c5d6b556c
Dominic Bourdua <dominic.bourdua@gmail.com>
2021-03-04 10:38:21 -0500

    OPAL-866 - Finished refactor Patient module

--- php/patient/get.patient_details.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../config.php");
-
-$serial = strip_tags($_POST['serial']);
-$patientObj = new Patient(); // Object
-$patientDetails = $patientObj->getPatientDetails($serial);
-
-header('Content-Type: application/javascript');
-echo json_encode($patientDetails);
