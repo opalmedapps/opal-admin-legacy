fdb7f89302be3dbdc750d3d15f6c67716ac6ffee
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2021-03-12 01:02:41 -0500

    recover lines missing

--- php/config.php
+++ php/config.php
@@ -78,6 +78,8 @@ define("MODULE_MASTER_SOURCE", 20);
 define("MODULE_SMS", 21);
 define("LOCAL_SOURCE_ONLY", -1);
 
+define("MODULE_PUBLICATION_TRIGGER",array(MODULE_QUESTIONNAIRE, MODULE_ALERT, MODULE_EDU_MAT, MODULE_POST));
+
 define("DELETED_RECORD", 1);
 define("NON_DELETED_RECORD", 0);
 define("NON_FINAL_RECORD", 0);
@@ -142,6 +144,7 @@ include_once( FRONTEND_ABS_PATH . "php". DIRECTORY_SEPARATOR . "classes". DIRECT
 include_once( FRONTEND_ABS_PATH . "php". DIRECTORY_SEPARATOR . "classes". DIRECTORY_SEPARATOR . "DatabaseAria.php" );
 include_once( FRONTEND_ABS_PATH . "php". DIRECTORY_SEPARATOR . "classes". DIRECTORY_SEPARATOR . "DatabaseOrms.php" );
 include_once( FRONTEND_ABS_PATH . "php". DIRECTORY_SEPARATOR . "classes". DIRECTORY_SEPARATOR . "DatabaseDisconnected.php" );
+include_once( FRONTEND_ABS_PATH . "php". DIRECTORY_SEPARATOR . "classes". DIRECTORY_SEPARATOR . "Trigger.php" );
 
 // Push Notification FCM and APN credientials.
 define( "API_KEY" , $config['pushNotificationConfig']['android']['apiKey'] );
@@ -216,6 +219,12 @@ if(!$ignoreSecuredConnection) {
         exit();
     }
 }
+
+define("ABVR_FRENCH_LANGUAGE", "FR");
+define("ABVR_ENGLISH_LANGUAGE", "EN");
+// all language abbreviations in opal admin
+define("OPAL_ADMIN_LANGUAGES",array(ABVR_FRENCH_LANGUAGE, ABVR_ENGLISH_LANGUAGE));
+
 
 /*
  * PHP Sessions config
