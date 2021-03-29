a561f8dbcbd0d045b93d1e8420d0bd20fc8aa954
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2021-03-29 11:18:54 -0400

    search filter

--- js/app/controllers/sms/sms.js
+++ js/app/controllers/sms/sms.js
@@ -5,9 +5,39 @@ angular.module('opalAdmin.controllers.sms', ['ngAnimate', 'ui.bootstrap', 'ui.gr
      * SMS Page controller
      *******************************************************************************/
     controller('sms', function ($scope, $uibModal, $filter, $state, smsCollectionService, uiGridConstants, Session, ErrorHandler, MODULE) {
-        $scope.test = "Welcome";
-        console.log($scope.test);
         $scope.navMenu = Session.retrieveObject('menu');
+        $scope.readAccess = ((parseInt(Session.retrieveObject('access')[MODULE.alias]) & (1 << 0)) !== 0);
+        $scope.writeAccess = ((parseInt(Session.retrieveObject('access')[MODULE.alias]) & (1 << 1)) !== 0);
+        $scope.deleteAccess = ((parseInt(Session.retrieveObject('access')[MODULE.alias]) & (1 << 2)) !== 0);
+
+        getSmsAppointmentList();
+
+        // Banner
+        $scope.bannerMessage = "";
+        // Function to show page banner
+        $scope.showBanner = function () {
+            $(".bannerMessage").slideDown(function () {
+                setTimeout(function () {
+                    $(".bannerMessage").slideUp();
+                }, 3000);
+            });
+        };
+
+        // Function to set banner class
+        $scope.setBannerClass = function (classname) {
+            // Remove any classes starting with "alert-"
+            $(".bannerMessage").removeClass(function (index, css) {
+                return (css.match(/(^|\s)alert-\S+/g) || []).join(' ');
+            });
+            // Add class
+            $(".bannerMessage").addClass('alert-' + classname);
+        };
+
+        $scope.filterSms = function (filterValue) {
+            $scope.filterValue = filterValue;
+            $scope.gridApi.grid.refresh();
+
+        };
 
         function buildOperations() {
             $scope.updatedRole = JSON.parse(JSON.stringify($scope.toSubmit));
@@ -45,10 +75,11 @@ angular.module('opalAdmin.controllers.sms', ['ngAnimate', 'ui.bootstrap', 'ui.gr
                 });
             }
         };
-        getSmsAppointmentList();
+
         function getSmsAppointmentList() {
             smsCollectionService.getsmsAppointments().then(function (response) {
                 $scope.smsAppoinments = response.data;
+                console.log($scope.smsAppoinments);
             }).catch(function(err) {
                 ErrorHandler.onError(err, $filter('translate')('SMS.LIST.ERROR_LIST'));
             });
