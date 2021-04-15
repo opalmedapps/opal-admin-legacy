11c7ce699d9cc98fd5d62daa3e25f9046301f093
Douzeyu <integrator@localhost.localdomain>
2021-04-15 00:53:56 -0400

    Message field and save message

--- js/app/controllers/sms/sms.message.js
+++ js/app/controllers/sms/sms.message.js
@@ -11,4 +11,55 @@ angular.module('opalAdmin.controllers.sms.message', ['ngAnimate', 'ui.bootstrap'
             window.history.back();
         };
 
+        $scope.message = {
+            en : "",
+            fr : "",
+        }
+        $scope.selected = {
+            speciality : "",
+            type : "",
+            event :"",
+        }
+
+        $scope.UpdateMessage = function(){
+            if ($scope.changesMade && $scope.writeAccess) {
+                $.ajax({
+                    type: "POST",
+                    url: "sms/update/smsMessage",
+                    data:"",
+                    success: function (response) {
+                        getSmsAppointmentList();
+                        response = JSON.parse(response);
+                        // Show success or failure depending on response
+                        if (response) {
+                            $scope.setBannerClass('success');
+                            $scope.bannerMessage = "success";
+                            $scope.showBanner();
+                        }
+                        else {
+                            ErrorHandler.onError(response, "error");
+                        }
+                    },
+                    error: function(err) {
+                        ErrorHandler.onError(err,"error");
+                    }
+                });
+            }
+        }
+
+        function getSmsAppointmentList() {
+            smsCollectionService.getsmsAppointments().then(function (response) {
+                response.data.forEach(function (row){
+                    switch (row.apptype){
+                        case null:
+                            row.apptype = 'UNDEFINED';
+                    }
+                    row.modified = 0;
+                })
+                $scope.smsAppointments = response.data;
+                console.log($scope.smsAppointments);
+            }).catch(function(err) {
+                ErrorHandler.onError(err, $filter('translate')('SMS.LIST.ERROR_LIST'));
+            });
+        }
     });
