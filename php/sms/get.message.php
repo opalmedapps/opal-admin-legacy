26c2cc65951e2359b7059a11d5032af0ca9cce94
Douzeyu <integrator@localhost.localdomain>
2021-04-16 01:57:29 -0400

    complete collection and fix Sms class

--- php/sms/get.message.php
+++ php/sms/get.message.php
@@ -1,12 +1,13 @@
 <?php
 
 include_once('sms.inc');
+$speciality = strip_tags($_POST['speciality']);
 $type = strip_tags($_POST['type']);
 $event = strip_tags($_POST['event']);
 $language = strip_tags($_POST['language']);
 
 $sms = new Sms();
-$sms->getMessage();
+$sms->getMessage($speciality,$type,$event,$language);
 
 header('Content-Type: application/javascript');
 http_response_code(HTTP_STATUS_SUCCESS);
