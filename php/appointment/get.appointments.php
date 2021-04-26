73ac2d0c6bcd02608790d7ebc62e9ce4224a8608
Yves Ferdinand <yves.ferdinand@muhc.mcgill.ca>
2021-04-26 17:47:36 -0400

    OPAL-891 - Get Appointment Information API

--- php/appointment/get.appointments.php
+++ php/appointment/get.appointments.php
@@ -1,8 +1,9 @@
 <?php
 
 include_once("../config.php");
+$data = json_decode(file_get_contents('php://input'), true);
 $appointment = new Appointment(); // Object
-$response = $appointment->getAppointment($_POST);
+$response = $appointment->getAppointment($data);
 header('Content-Type: application/javascript');
 print json_encode($response); // Return response
 
