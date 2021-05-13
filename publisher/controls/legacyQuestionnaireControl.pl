35c491271223821eba421d6e6d77b99233e7dcfa
napparific <kelly.agnew@mail.mcgill.ca>
2021-05-13 16:09:31 -0400

    Add controls for outgoing-information modules

---
+++ publisher/controls/legacyQuestionnaireControl.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+#---------------------------------------------------------------------------------
+# K.Agnew 2021 Cron Modularity Refactor ++
+#---------------------------------------------------------------------------------
+# Control script for the sending of questionnaires to patients. This script has its
+# own dedicated cron job.
+# We use our custom Perl Modules to help us with getting information and
+# setting them into the appropriate place.
+
+
+#-----------------------------------------------------------------------
+# Packages/Modules
+#-----------------------------------------------------------------------
+use Time::Piece;
+use POSIX;
+use Storable qw(dclone);
+use File::Basename;
+use File::Spec;
+use JSON;
+use MIME::Lite;
+use Data::Dumper;
+use Net::Address::IP::Local;
+use Cwd 'abs_path';
+
+
+#====================================================================================
+# Helper Subroutine to log json content to a file
+#====================================================================================
+sub writeToLogFile
+{
+	my ($monitor_log, $contents, $writeOption) = @_; # args
+	open my $file_handler, $writeOption, $monitor_log;
+	print $file_handler $contents;
+	close $file_handler;
+
+	return;
+}
+
