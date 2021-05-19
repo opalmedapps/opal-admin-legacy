f3db360984c774ac92fec95e24cb61288181e3b9
napparific <kelly.agnew@mail.mcgill.ca>
2021-05-19 10:39:26 -0400

    Add script execution monitor for each control

--- publisher/controls/legacyQuestionnaireControl.pl
+++ publisher/controls/legacyQuestionnaireControl.pl
@@ -33,6 +33,177 @@ use EducationalMaterialControl;
 use LegacyQuestionnaire;
 use Cron;
 
+#-----------------------------------------------------------------------
+# Monitor this script's execution
+# - What this section does is
+# 	1. Checks for hanging jobs
+# 	2. Checks for crash reports
+# Both recorded in monitor_log.json
+#-----------------------------------------------------------------------
+
+our $wsSlash = File::Spec->catfile('', '');
+
+$execution_log = dirname($0) . '/logs/executions.log';
+$monitor_log = dirname($0) . '/logs/monitor_log.json';
+
+my $this_script = abs_path($0);
+my $ip_address = Net::Address::IP::Local->public;
+my $json_log;
+if (-e $monitor_log) { # file exists
+	{
+	  local $/; # Enable 'slurp' mode
+	  open my $file_handler, "<", $monitor_log;
+	  $json_log = <$file_handler>;
+	  close $file_handler;
+	}
+	$json_log = decode_json($json_log); # get json
+
+	my $pid = $json_log->{'pid'}; # process id
+	# check if process in file is running
+	system("ps -p $pid > /dev/null 2>&1");
+
+	if ($? eq 0) { # this script is already running
+
+		my $run_count = $json_log->{'run'}->{'count'}; # get execution count
+		$run_count++;
+		$json_log->{'run'}->{'count'} = $run_count; # to set back in file
+
+		if ($run_count % 10 eq 9) { # email execution report every 10 times
+
+			unless(-e $execution_log) {
+			    # Create the file if it doesn't exist
+			    open my $file_handler, ">", $execution_log;
+			    close $file_handler;
+			}
+			open(my $file_handler, "<", $execution_log);
+			my @lines = reverse <$file_handler>; # read file from tail
+			my @logs;
+			push (@logs, "IP: <strong>$ip_address</strong> at <strong>" . $this_script . "</strong><br>"); # push first line
+			my $count = 0;
+			foreach $line (@lines) {
+				push (@logs, "$line<br>");
+				$count++;
+				if ($count > 50) { last; } # only read last 50 lines
+			}
+
+			# email error
+			my $mime = MIME::Lite->new(
+				'From'		=> "opal\@muhc.mcgill.ca",
+				'To'		=> "ackeem.berry\@gmail.com",
+				'Cc'			=> "yickmo\@gmail.com",
+				'Subject'	=> "Potential hanging script - Opal dataControl.pl",
+				'Type'		=> 'text/html',
+				'Data'		=> \@logs,
+			);
+
+			my $response = $mime->send('smtp', '172.25.123.208');
+
+			# set last emailed date
+			$json_log->{'run'}->{'last_emailed'} = strftime("%Y-%m-%d %H:%M:%S", localtime(time)); # now
+
+		}
+
+		# write back to file
+		writeToLogFile($monitor_log, encode_json($json_log), ">");
+
+		exit 0; # quit this script since already running on another process
+	}
+	else { # new process
+
+		my $pid = $$; # get process id
+
+		my $start_flag = $json_log->{'start'}; # get start flag from file
+		if ($start_flag ne 0) { # script has been crashing
+
+			$json_log->{'pid'} = $pid; # set pid
+
+			my $crash_count = $json_log->{'crash'}->{'count'}; # get crash count
+			$crash_count++;
+			$json_log->{'crash'}->{'count'} = $crash_count; # to set crash count back to file
+
+			if ($crash_count % 10 eq 9) { # email crash report after 10 crashes
+
+				unless(-e $execution_log) {
+				    #Create the file if it doesn't exist
+				    open my $file_handler, ">", $execution_log;
+				    close $file_handler;
+				}
+				open(my $file_handler, "<", $execution_log);
+				my @lines = reverse <$file_handler>; # read file from tail
+				my @logs;
+				push (@logs, "IP: <strong>$ip_address</strong> at <strong>" . $this_script . "</strong><br>"); # push first line
+				my $count = 0;
+				foreach $line (@lines) {
+					push (@logs, "$line<br>");
+					$count++;
+					if ($count > 50) { last; } # only get last 50 lines
+				}
+
+				# email error
+				my $mime = MIME::Lite->new(
+					'From'		=> "opal\@muhc.mcgill.ca",
+					'To'		=> "ackeem.berry\@gmail.com",
+					'Cc'			=> "yickmo\@gmail.com",
+					'Subject'	=> "Script crash - Opal dataControl.pl",
+					'Type'		=> 'text/html',
+					'Data'		=> \@logs,
+				);
+
+				my $response = $mime->send('smtp', '172.25.123.208');
+
+				# set last emailed date
+				$json_log->{'crash'}->{'last_emailed'} = strftime("%Y-%m-%d %H:%M:%S", localtime(time)); # now
+			}
+
+
+		}
+		else { # clean new process
+
+			# flush/new log
+			$json_log = {
+				'pid'	=> $pid,
+				'start'	=> 0,
+				'run'	=> {
+					'count'	=> 0,
+					'last_emailed' 	=> 0
+				},
+				'crash'	=> {
+					'count'	=> 0,
+					'last_emailed'	=> 0
+				}
+			};
+		}
+
+		writeToLogFile($monitor_log, encode_json($json_log), ">");
+	}
+}
+else { # log file DNE
+
+	# to write new process
+	my $pid = $$;
+	$json_log = {
+		'pid'	=> $pid,
+		'start'	=> 0,
+		'run'	=> {
+			'count'	=> 0,
+			'last_emailed' 	=> 0
+		},
+		'crash'	=> {
+			'count'	=> 0,
+			'last_emailed'	=> 0
+		}
+	};
+
+	# write new file
+	writeToLogFile($monitor_log, encode_json($json_log), ">");
+
+}
+
+# set start flag to signify script execution
+$json_log->{'start'} = 1;
+writeToLogFile($monitor_log, encode_json($json_log), ">");
+
+
 #====================================================================================
 # Helper Subroutine to log json content to a file
 #====================================================================================
