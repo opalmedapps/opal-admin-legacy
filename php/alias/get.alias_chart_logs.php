2755865f112bbb7b0d7f11e5731b6f796c8ac29c
Dominic Bourdua <dominic.bourdua@muhc.mcgill.ca>
2021-05-27 14:58:13 -0400

    opal-871 - refactored alias log. Refactoring deletion underway.

--- php/alias/get.alias_chart_logs.php
+++ php/alias/get.alias_chart_logs.php
@@ -1,19 +1,9 @@
 <?php
-	header('Content-Type: application/javascript');
-	/* To get logs on a particular alias for highcharts */
-	include_once('alias.inc');
 
-	// Retrieve FORM params
-	$callback = $_POST['callback'];
-	$serial = ( $_POST['serial'] === 'undefined' ) ? null : $_POST['serial'];
-	$type = ( $_POST['type'] === 'undefined' ) ? null : $_POST['type'];
+include_once("../config.php");
 
-	$alias = new Alias; // Object
+$alias = new Alias(); // Object
+$aliasLogs = $alias->getAliasChartLogs($_POST);
 
-	// Call function
-	$aliasLogs = $alias->getAliasChartLogs($serial, $type);
-
-	// Callback to http request
-	echo json_encode($aliasLogs);
-
-?>
+header('Content-Type: application/javascript');
+echo json_encode($aliasLogs);
--- php/alias/update.alias_publish_flags.php
+++ php/alias/update.alias_publish_flags.php
@@ -3,7 +3,7 @@
 include_once("../config.php");
 
 $aliasObject = new Alias();
-$response = $aliasObject->updateAliasPublishFlags($_POST);
+$aliasObject->updateAliasPublishFlags($_POST);
 
 header('Content-Type: application/javascript');
 http_response_code(HTTP_STATUS_SUCCESS);
