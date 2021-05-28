6bb3888957d8e968368aa270d84916e14137c55f
Dominic Bourdua <dominic.bourdua@muhc.mcgill.ca>
2021-05-28 08:14:24 -0400

    opal-871 - removed delete alias operations/methods

--- php/alias/delete.alias.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once("../config.php");
-
-$alias = new Alias();
-$response = $alias->deleteAlias($_POST);
-
-header('Content-Type: application/javascript');
-http_response_code(HTTP_STATUS_SUCCESS);
