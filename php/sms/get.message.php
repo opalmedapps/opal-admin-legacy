1d10acd57877167f6022a6be0dfaa1671bcb7914
Douzeyu <integrator@localhost.localdomain>
2021-06-01 19:55:22 -0400

    rewrite sms class

--- php/sms/get.message.php
+++ php/sms/get.message.php
@@ -1,14 +1,8 @@
 <?php
 header('Content-Type: application/javascript');
 include_once("../config.php");
-$speciality = strip_tags($_POST['speciality']);
-$type = strip_tags($_POST['type']);
-$event = strip_tags($_POST['event']);
-$language = strip_tags($_POST['language']);
-
 
 $sms = new Sms();
-$smsMessage = $sms->getMessage($speciality,$type,$event,$language);
+$smsMessage = $sms->getMessage($_POST);
 
 echo json_encode($smsMessage);
-//http_response_code(HTTP_STATUS_SUCCESS);
--- php/sms/update.smsMessage.php
+++ php/sms/update.smsMessage.php
@@ -2,9 +2,7 @@
 header('Content-Type: application/javascript');
 include_once("../config.php");
 
-$messageUpdates	= strip_tags($_POST['UpdateInformation']);
-
 $sms = new Sms();
-$response = $sms->updateSmsMessage($messageUpdates);
+$response = $sms->updateSmsMessage($_POST);
 
 echo json_encode($response);
