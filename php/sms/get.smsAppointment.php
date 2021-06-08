a596a4deb636716642181603fe0f223ea5662aba
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2021-06-07 21:29:58 -0400

    more comments

--- php/sms/get.smsAppointment.php
+++ php/sms/get.smsAppointment.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/javascript');
 include_once("../config.php");
 
-$sms = new Sms();
+$sms = new Sms(); // Object
 $smsAppointmentList = $sms->getAppointments();
 
 echo json_encode($smsAppointmentList);
--- php/sms/update.smsMessage.php
+++ php/sms/update.smsMessage.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/javascript');
 include_once("../config.php");
 
-$sms = new Sms();
+$sms = new Sms(); // Object
 $response = $sms->updateSmsMessage($_POST);
 
 echo json_encode($response);
