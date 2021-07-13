14f72902c5a5773ec6a802a04f4887646985b449
napparific <kelly.agnew@mail.mcgill.ca>
2021-07-13 11:30:55 -0400

    Remove dead code

--- publisher/modules/Announcement.pm
+++ publisher/modules/Announcement.pm
@@ -157,17 +157,6 @@ sub publishAnnouncements
 
     #my $today_date = strftime("%Y-%m-%d", localtime(time));
     my $now = Time::Piece->strptime(strftime("%Y-%m-%d %H:%M:%S", localtime(time)), "%Y-%m-%d %H:%M:%S");
-
-	#Cron refactor 2021-02 removing the time check and replacing with a time check in HosptialPushNotification.php
-
-    # Date object of today at 8AM
-    #my $today_at_eightAM = Time::Piece->strptime($today_date . " 08:00:00", "%Y-%m-%d %H:%M:%S");
-    # Date object of today at 8PM
-    #my $today_at_eightPM = Time::Piece->strptime($today_date . " 20:00:00", "%Y-%m-%d %H:%M:%S");
-
-    # If we are not within the window to publish announcements then return
-    #if ( (($now - $today_at_eightAM) < 0) or (($now - $today_at_eightPM) > 0) ) {return;}
-
 	
 	# Check for any new updates from the main cron control
 	PostControl::CheckPostControlsMarkedForPublishModularCron('Announcement');
