84f3f537431da5ebf291080e55452458c93b5f7b
napparific <kelly.agnew@mail.mcgill.ca>
2021-07-16 15:45:10 -0400

    Remove PatientId from PatientDoctor varian query

--- publisher/modules/ResourceAppointment.pm
+++ publisher/modules/ResourceAppointment.pm
@@ -365,20 +365,6 @@ sub getResourceAppointmentsFromSourceDB
 			my $numOfPatients = @patientList;
 			my $counter = 0;
 			my $databaseName = $Configs::OPAL_DB_NAME;
-			# foreach my $Patient (@patientList) {
-				# my $patientSer 			= $Patient->getPatientSer();
-				# my $patientSSN          = $Patient->getPatientSSN(); # get ssn
-				# my $patientLastTransfer	= $Patient->getPatientLastTransfer(); # get last updated
-
-				# $patientInfo_sql .= "
-					# SELECT '$patientSSN' as SSN, '$patientLastTransfer' as LastTransfer, '$patientSer' as PatientSerNum
-				# ";
-
-				# $counter++;
-				# if ( $counter < $numOfPatients ) {
-					# $patientInfo_sql .= "UNION";
-				# }
-			# }
 
 			my $raInfo_sql = "
 				SELECT DISTINCT
