a3d7016c6fe613f0330834552796326e3a970840
Dominic Bourdua <dominic.bourdua@gmail.com>
2021-10-20 10:20:36 -0400

    OPAL-1138 - centralized post validation

--- php/post/delete.post.php
+++ php/post/delete.post.php
@@ -4,7 +4,7 @@ include_once("../config.php");
 $postId = strip_tags($_POST["serial"]);
 
 $postObject = new Post();
-$response = $postObject->deletePost($postId);
+$postObject->deletePost($postId);
 
 header('Content-Type: application/javascript');
 http_response_code(HTTP_STATUS_SUCCESS);
--- php/post/insert.post.php
+++ php/post/insert.post.php
@@ -1,14 +1,8 @@
 <?php
 include_once("../config.php");
 
-$sessionId = strip_tags($_POST["OAUser"]["sessionid"]);
-
-$sanitizedPost = Post::validateAndSanitize($_POST);
-if(!$sanitizedPost)
-    HelpSetup::returnErrorMessage(HTTP_STATUS_INTERNAL_SERVER_ERROR, "Invalid post format");
-
 $postObject = new Post();
-$postObject->insertPost($sanitizedPost);
+$postObject->insertPost($_POST);
 
 header('Content-Type: application/javascript');
 http_response_code(HTTP_STATUS_SUCCESS);
