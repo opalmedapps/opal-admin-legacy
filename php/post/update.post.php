79c91758380046ddb3efbf44f297fd94e9678c6b
Dominic Bourdua <dominic.bourdua@gmail.com>
2021-10-20 10:21:13 -0400

    OPAL-1138 - centralized post validation - bug fixed

--- php/post/update.post.php
+++ php/post/update.post.php
@@ -1,10 +1,8 @@
 <?php
 include_once("../config.php");
 
-
-
 $postObject = new Post();
-$response = $postObject->updatePost($sanitizedPost);
+$response = $postObject->updatePost($_POST);
 
 header('Content-Type: application/javascript');
 http_response_code(HTTP_STATUS_SUCCESS);
