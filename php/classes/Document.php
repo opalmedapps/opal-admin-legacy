a9099cc454764b513370a24983d8c77d30029747
Yves Ferdinand <yves.ferdinand@gmail.com>
2021-11-09 04:28:53 -0500

    Add Document

--- php/classes/Document.php
+++ php/classes/Document.php
@@ -15,7 +15,7 @@ class Document extends Module
     protected function _validateDocumentSourceExternalId(&$post, &$patientSite, &$source)  {
         $patientSite = array();
         $errCode = $this->_validateBasicPatientInfo($post, $patientSite);
-        
+                
         // 4th bit - source
         if(!array_key_exists("sourceSystem", $post) || $post["sourceSystem"] == "") {
             $errCode = "1" . $errCode;
@@ -29,8 +29,7 @@ class Document extends Module
                 $source = $source[0];
                 $errCode = "0" . $errCode;
             }
-        }
-
+        }        
         return $errCode;
     }
 
@@ -41,11 +40,94 @@ class Document extends Module
         if($errCode != 0)
             HelpSetup::returnErrorMessage(HTTP_STATUS_BAD_REQUEST_ERROR, json_encode(array("validation"=>$errCode)));
 
-        //bit 
-        if(!array_key_exists("patientVisitId", $post) || $post["patientVisitId"] == ""){
+        //bit 5
+        if(!array_key_exists("documentId", $post) || $post["documentId"] == ""){
             $errCode = "1" . $errCode;
         } else{
             $errCode = "0" . $errCode;
+        }
+
+        //bit 6
+        if(!array_key_exists("appovalUserId", $post) || $post["appovalUserId"] == ""){
+            $errCode = "1" . $errCode;
+        } else{
+            $errCode = "0" . $errCode;
+        }
+
+        //bit 7
+        if(array_key_exists("approvalDatetime", $post) && $post["approvalDatetime"] != "") {
+            if(!HelpSetup::verifyDate($post["approvalDatetime"], false, 'Y-m-d H:i:s'))
+                $errCode = "1" . $errCode;
+            else {                
+                $errCode = "0" . $errCode;
+            }
+        }
+
+        //bit 8
+        if(!array_key_exists("authorUserId", $post) || $post["authorUserId"] == ""){
+            $errCode = "1" . $errCode;
+        } else{
+            $errCode = "0" . $errCode;
+        }
+
+        //bit 9
+        if(!array_key_exists("noteDescription", $post) || $post["noteDescription"] == ""){
+            $errCode = "1" . $errCode;
+        } else{
+            $errCode = "0" . $errCode;
+        }
+
+        //bit 10
+        if(array_key_exists("noteDatetime", $post) && $post["noteDatetime"] != "") {
+            if(!HelpSetup::verifyDate($post["noteDatetime"], false, 'Y-m-d H:i:s'))
+                $errCode = "1" . $errCode;
+            else {                
+                $errCode = "0" . $errCode;
+            }
+        }
+
+        //bit 11
+        if(!array_key_exists("revised", $post) || $post["revised"] == ""){
+            $errCode = "1" . $errCode;
+        } else{
+            $errCode = "0" . $errCode;
+        }
+
+        //bit 12
+        if(!array_key_exists("validEntry", $post) || $post["validEntry"] == ""){
+            $errCode = "1" . $errCode;
+        } else{
+            $errCode = "0" . $errCode;
+        }
+
+        //bit 13
+        if(!array_key_exists("errorMessage", $post) || $post["errorMessage"] == ""){
+            $errCode = "1" . $errCode;
+        } else{
+            $errCode = "0" . $errCode;
+        }
+
+        //bit 14
+        if(!array_key_exists("fileName", $post) || $post["fileName"] == ""){
+            $errCode = "1" . $errCode;
+        } else{
+            $errCode = "0" . $errCode;
+        }
+
+        //bit 15
+        if(!array_key_exists("creationUserId", $post) || $post["creationUserId"] == ""){
+            $errCode = "1" . $errCode;
+        } else{
+            $errCode = "0" . $errCode;
+        }
+
+        //bit 16
+        if(array_key_exists("creationDatetime", $post) && $post["creationDatetime"] != "") {
+            if(!HelpSetup::verifyDate($post["creationDatetime"], false, 'Y-m-d H:i:s'))
+                $errCode = "1" . $errCode;
+            else {                
+                $errCode = "0" . $errCode;
+            }
         }
 
         return $errCode;
@@ -53,7 +135,7 @@ class Document extends Module
 
     protected function _insertDocument($post){
         $patientSite = null;
-        $source = null;
+        $source = null;        
         $errCode = $this->_validateInsertDocument($post, $patientSite, $source);
         $errCode = bindec($errCode);
         if ($errCode != 0)
@@ -62,7 +144,7 @@ class Document extends Module
         $toInsert = array(
             "PatientSerNum" => $patientSite["PatientSerNum"],
             "SourceDatabaseSerNum" => $source["SourceDatabaseSerNum"],
-            "DocumentId" => $post["uuid"],
+            "DocumentId" => $post["documentId"],
             "ApprovedBySerNum" => $post["appovalUserId"],
             "ApprovedTimeStamp" => $post["approvalDatetime"],
             "AuthoredBySerNum" => $post["authorUserId"],
@@ -77,17 +159,33 @@ class Document extends Module
             "TransferStatus" => "T",
             "TransferLog" => "",
             "ReadStatus" => 0,
-            "SessionId" => $this->opalDB->getSessionId(),
-            "DateAdded" => date("Y-m-d H:i:s")            
+            "SessionId" => $this->opalDB->getSessionId()
         );
 
         $aliasInfos = $this->opalDB->getAlias('Document',$post['noteDescription'], $post['noteDescription']);
-        if(count($aliasInfos) == 1) {
-            $toInsert["AliasExpressionSerNum"] = $aliasInfos["AliasExpressionSerNum"];
+        
+        if(count($aliasInfos) == 1) {            
+            $toInsert["AliasExpressionSerNum"] = $aliasInfos[0]["AliasExpressionSerNum"];
         }
 
-        
+        $doc = $this->opalDB->getDocument($source["SourceDatabaseSerNum"], $post["documentId"]);        
 
+        if (count($doc) == 0) {
+            $toInsert["DateAdded"] = date("Y-m-d H:i:s");            
+            $id = $this->opalDB->insertDocument($toInsert);                    
+            var_dump($id);
+        } else {
+            $toInsert["LastUpdated"] = $post["modifiedDatetime"];
+        }
+
+        if ($post["validEntry"] == "Y"  && array_key_exists("documentString", $post) && $post["documentString"] != "")  {
+            $filename = basename($post["fileName"]);
+            $output_file = "/OpalDocuments/clinical/documents/" . $filename;            
+            $ifp = fopen ( $output_file, "w+") or die("Unable to open file!");            
+            $data = explode( ',', $post["documentString"]);
+            fwrite ( $ifp, base64_decode( $data[0] ) );            
+            fclose( $ifp );            
+        }
     }
     /*
      * Insert a new document after validation.
@@ -95,7 +193,7 @@ class Document extends Module
      * @return  200 or error 400 with validation error
      * */
     public function insertDocument($post) {
-        $this->checkWriteAccess($post);
+        $this->checkWriteAccess($post);        
         $post = HelpSetup::arraySanitization($post);
         return $this->_insertDocument($post);
     }
