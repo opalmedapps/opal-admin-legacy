6dac23013b5b4250721ccdfba0d049ecbb7ad745
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2021-11-26 08:28:31 -0500

    Create firebase class

--- php/patient/modification-tools/update.email.php
+++ php/patient/modification-tools/update.email.php
@@ -2,3 +2,11 @@
 
 header('Content-Type: application/javascript');
 include_once('patient.inc');
+
+$uid = $_POST["userId"];
+$newPassword = $_POST["new_password"];
+
+$myFirebase = new Firebase();
+//'b0tEHXqDqwN9s7qKQdX1SqdTIQm1', "testpassword1234!"
+
+print json_encode($myFirebase->changePassword($uid, $newPassword));
