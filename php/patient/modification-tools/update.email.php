06a6a9d4dc2fbbb0c6be2bc225e9508aa10f9d69
Douzeyu <integrator@10.0.2.15>
2021-12-06 17:23:58 -0500

    build php scripts for modification tool

--- php/patient/modification-tools/update.email.php
+++ php/patient/modification-tools/update.email.php
@@ -1,12 +1,14 @@
 <?php
 
 header('Content-Type: application/javascript');
-include_once('patient.inc');
+include_once("../../config.php");
 
-$uid = $_POST["userId"];
-$newPassword = $_POST["new_password"];
-
+$patientObj = new Patient();
+$uid = $patientObj->getPatientUsername($_POST);
 $myFirebase = new Firebase();
 //'b0tEHXqDqwN9s7qKQdX1SqdTIQm1', "testpassword1234!"
 
-print json_encode($myFirebase->changePassword($uid, $newPassword));
+$myFirebase->changeEmail($uid, $_POST);
+$response = $patientObj->updatePatientEmail($_POST);
+
+print json_encode($response);
