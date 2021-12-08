5c0f7fb11353b7254942641ecf55924befef5d9c
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2021-12-07 19:30:59 -0500

    rename the files

--- js/app/controllers/patient/modification-tools/update.security.answer.js
+++
@@ -1,3 +0,0 @@
-angular.module('opalAdmin.controllers.update.security.answer', ['ngAnimate', 'ui.bootstrap', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.autoResize']).controller('update.security.answer', function ($scope, $rootScope, Session, ErrorHandler, MODULE, $uibModal, $filter) {
-
-});
