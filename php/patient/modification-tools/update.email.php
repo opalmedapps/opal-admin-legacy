2816b9172182b754930e79f029bba1d961e7b3b2
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2021-12-28 23:02:24 -0500

    change search page and email modal

--- php/patient/modification-tools/update.email.php
+++ php/patient/modification-tools/update.email.php
@@ -2,13 +2,12 @@
 
 header('Content-Type: application/javascript');
 include_once("../../config.php");
+require __DIR__.'/../../../vendor/autoload.php';
 
 $patientObj = new Patient();
-$uid = $patientObj->getPatientUsername($_POST);
 $myFirebase = new Firebase();
 //'b0tEHXqDqwN9s7qKQdX1SqdTIQm1', "testpassword1234!"
 
-$myFirebase->changeEmail($uid, $_POST);
-$response = $patientObj->updatePatientEmail($_POST);
-
-print json_encode($response);
+$myFirebase->changeEmail($_POST);
+$patientObj->updatePatientEmail($_POST);
+http_response_code(HTTP_STATUS_SUCCESS);
