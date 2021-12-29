3def2f4a20842415ab4104ef7c1781580c9ca9cf
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2021-12-28 23:02:24 -0500

    change search page and email modal

--- js/app/controllers/patient/modification-tools/update.email.js
+++ js/app/controllers/patient/modification-tools/update.email.js
@@ -1,3 +1,42 @@
-angular.module('opalAdmin.controllers.update.email', ['ngAnimate', 'ui.bootstrap', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.autoResize']).controller('update.email', function ($scope, $rootScope, Session, ErrorHandler, MODULE, $uibModal, $filter) {
+angular.module('opalAdmin.controllers.update.email', ['ngAnimate', 'ui.bootstrap', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.autoResize']).controller('update.email', function ($scope, $filter, $uibModal, $uibModalInstance, patientCollectionService, $state, Session, ErrorHandler) {
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    $scope.new_email = {
+        firstTime: null,
+        secondTime: null,
+        errorMessage: null,
+    };
+
+    var arrValidationInsert = [
+        $filter('translate')('SMS.VALIDATION.APPOINTMENT_ID'),
+        $filter('translate')('SMS.VALIDATION.ACTIVE'),
+        $filter('translate')('SMS.VALIDATION.TYPE'),
+        $filter('translate')('SMS.VALIDATION.UNDEFINED_ACTIVE'),
+    ];
+
+    $scope.updateEmail = function(){
+        if($scope.new_email.firstTime !== null && $scope.new_email.secondTime !== null && $scope.new_email.errorMessage === null){
+            $.ajax({
+                type: "POST",
+                url: "patient/update/email",
+                data: {
+                    uid: "b0tEHXqDqwN9s7qKQdX1SqdTIQm1",
+                    email: $scope.new_email.firstTime,
+                    PatientSerNum: $scope.psnum,
+                },
+                success: function () {},
+                error: function (err) {
+                    err.responseText = JSON.parse(err.responseText);
+                    ErrorHandler.onError(err, $filter('translate')('SMS.EDIT.ERROR'), arrValidationInsert);
+                },
+                complete: function () {
+                    $uibModalInstance.close();
+                }
+            });
+        }
+    }
 
 });
