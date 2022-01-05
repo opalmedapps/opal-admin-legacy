5a70589e91a6bf724c1a81ee5c494632f4a97721
Douzeyu <integrator@10.0.2.15>
2022-01-05 00:31:02 -0500

    debug and refactor email and password module

--- js/app/controllers/patient/modification-tools/update.email.js
+++ js/app/controllers/patient/modification-tools/update.email.js
@@ -10,33 +10,70 @@ angular.module('opalAdmin.controllers.update.email', ['ngAnimate', 'ui.bootstrap
         errorMessage: null,
     };
 
-    var arrValidationInsert = [
-        $filter('translate')('SMS.VALIDATION.APPOINTMENT_ID'),
-        $filter('translate')('SMS.VALIDATION.ACTIVE'),
-        $filter('translate')('SMS.VALIDATION.TYPE'),
-        $filter('translate')('SMS.VALIDATION.UNDEFINED_ACTIVE'),
+    $scope.validateEmail = function(){
+        if($scope.validateInput($scope.new_email.firstTime) && !$scope.new_email.firstTime.match(/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/))
+        {
+            $scope.new_email.errorMessage = $filter('translate')('PATIENTS.MODIFICATION_TOOLS.EMAIL.EMAIL_NOT_VALID');
+        }
+        else if($scope.validateInput($scope.new_email.firstTime) && $scope.validateInput($scope.new_email.secondTime) && $scope.new_email.firstTime !== $scope.new_email.secondTime)
+        {
+            $scope.new_email.errorMessage = $filter('translate')('PATIENTS.MODIFICATION_TOOLS.EMAIL.EMAIL_NOT_SAME');
+        }
+        else
+        {
+            $scope.new_email.errorMessage = null;
+        }
+    }
+
+    var arrValidationUpdateDatabase = [
+        $filter('translate')('PATIENTS.MODIFICATION_TOOLS.VALIDATION.PATIENTSERNUM'),
+        $filter('translate')('PATIENTS.MODIFICATION_TOOLS.VALIDATION.EMAIL'),
+    ];
+
+    var arrValidationUpdateFirebase = [
+        $filter('translate')('PATIENTS.MODIFICATION_TOOLS.VALIDATION.USERID'),
+        $filter('translate')('PATIENTS.MODIFICATION_TOOLS.VALIDATION.EMAIL'),
     ];
 
     $scope.updateEmail = function(){
         if($scope.new_email.firstTime !== null && $scope.new_email.secondTime !== null && $scope.new_email.errorMessage === null){
             $.ajax({
                 type: "POST",
-                url: "patient/update/email",
+                url: "firebase/update/email",
                 data: {
                     uid: "b0tEHXqDqwN9s7qKQdX1SqdTIQm1",
                     email: $scope.new_email.firstTime,
-                    PatientSerNum: $scope.psnum,
                 },
-                success: function () {},
+                success: function () {
+                    $scope.updateEmailInDatabase();
+                },
                 error: function (err) {
-                    err.responseText = JSON.parse(err.responseText);
-                    ErrorHandler.onError(err, $filter('translate')('SMS.EDIT.ERROR'), arrValidationInsert);
+                    ErrorHandler.onError(err, $filter('translate')('PATIENTS.MODIFICATION_TOOLS.EMAIL.ERROR'), arrValidationUpdateFirebase);
                 },
                 complete: function () {
                     $uibModalInstance.close();
                 }
             });
         }
+    }
+
+    $scope.updateEmailInDatabase = function(){
+        $.ajax({
+            type: "POST",
+            url: "patient/update/email",
+            data: {
+                email: $scope.new_email.firstTime,
+                PatientSerNum: $scope.psnum,
+            },
+            success: function () {},
+            error: function (err) {
+                ErrorHandler.onError(err, $filter('translate')('PATIENTS.MODIFICATION_TOOLS.EMAIL.ERROR'), arrValidationUpdateDatabase);
+            },
+        });
+    }
+
+    $scope.validateInput = function(input){
+        return (input !== undefined && input !== null && input !== "");
     }
 
 });
--- php/patient/modification-tools/update.email.php
+++ php/patient/modification-tools/update.email.php
@@ -2,12 +2,8 @@
 
 header('Content-Type: application/javascript');
 include_once("../../config.php");
-require __DIR__.'/../../../vendor/autoload.php';
 
 $patientObj = new Patient();
-$myFirebase = new Firebase();
-//'b0tEHXqDqwN9s7qKQdX1SqdTIQm1', "testpassword1234!"
 
-$myFirebase->changeEmail($_POST);
 $patientObj->updatePatientEmail($_POST);
 http_response_code(HTTP_STATUS_SUCCESS);
