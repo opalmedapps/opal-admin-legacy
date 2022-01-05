dd753e1ae36310226482f730ccf0cebae6dd0852
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2022-01-05 00:31:02 -0500

    debug and refactor email and password module

--- js/app/controllers/patient/modification-tools/update.password.js
+++ js/app/controllers/patient/modification-tools/update.password.js
@@ -1,3 +1,78 @@
-angular.module('opalAdmin.controllers.update.password', ['ngAnimate', 'ui.bootstrap', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.autoResize']).controller('update.password', function ($scope, $rootScope, Session, ErrorHandler, MODULE, $uibModal, $filter) {
+angular.module('opalAdmin.controllers.update.password', ['ngAnimate', 'ui.bootstrap', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.autoResize']).controller('update.password', function ($scope, $filter, $uibModal, $uibModalInstance, patientCollectionService, $state, Session, ErrorHandler) {
+
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+
+    $scope.new_password = {
+        firstTime: null,
+        secondTime: null,
+        errorMessage: null,
+    };
+
+    $scope.validatePassword = function(){
+        if($scope.validateInput($scope.new_password.firstTime) && $scope.new_password.firstTime.length < 8)
+        {
+            $scope.new_password.errorMessage = $filter('translate')('PATIENTS.MODIFICATION_TOOLS.PASSWORD.PASSWORD_TOO_SHORT');
+        }
+        else if($scope.validateInput($scope.new_password.firstTime)&& (!$scope.new_password.firstTime.match(/\W|_{1}/) || !$scope.new_password.firstTime.match(/[A-Z]/) || !$scope.new_password.firstTime.match(/\d/)))
+        {
+            $scope.new_password.errorMessage = $filter('translate')('PATIENTS.MODIFICATION_TOOLS.PASSWORD.PASSWORD_MISSING_CHARACTER');
+        }
+        else if($scope.validateInput($scope.new_password.firstTime) && $scope.validateInput($scope.new_password.secondTime) && $scope.new_password.firstTime !== $scope.new_password.secondTime)
+        {
+            $scope.new_password.errorMessage = $filter('translate')('PATIENTS.MODIFICATION_TOOLS.PASSWORD.PASSWORD_NOT_SAME');
+        }
+        else
+        {
+            $scope.new_password.errorMessage = null;
+        }
+    }
+
+    var arrValidationUpdate = [
+        $filter('translate')('PATIENTS.MODIFICATION_TOOLS.VALIDATION.USERID'),
+        $filter('translate')('PATIENTS.MODIFICATION_TOOLS.VALIDATION.PASSWORD'),
+    ];
+
+    $scope.updatePassword = function(){
+        if($scope.new_password.firstTime !== null && $scope.new_password.secondTime !== null && $scope.new_password.errorMessage === null){
+            $.ajax({
+                type: "POST",
+                url: "firebase/update/password",
+                data: {
+                    uid: "b0tEHXqDqwN9s7qKQdX1SqdTIQm1",
+                    password: $scope.new_password.firstTime,
+                },
+                success: function () {
+                    $scope.updatePasswordInDatabase();
+                },
+                error: function (err) {
+                    ErrorHandler.onError(err, $filter('translate')('PATIENTS.MODIFICATION_TOOLS.PASSWORD.ERROR'), arrValidationUpdate);
+                },
+                complete: function () {
+                    $uibModalInstance.close();
+                }
+            });
+        }
+    }
+
+    $scope.updatePasswordInDatabase = function(){
+        $.ajax({
+            type: "POST",
+            url: "patient/update/password",
+            data: {
+                uid: "b0tEHXqDqwN9s7qKQdX1SqdTIQm1",
+                password: $scope.new_password.firstTime,
+            },
+            success: function () {},
+            error: function (err) {
+                ErrorHandler.onError(err, $filter('translate')('PATIENTS.MODIFICATION_TOOLS.PASSWORD.ERROR'), arrValidationUpdate);
+            },
+        });
+    }
+
+    $scope.validateInput = function(input){
+        return (input !== undefined && input !== null && input !== "");
+    }
 
 });
