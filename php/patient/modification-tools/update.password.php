e0735473fd0da525e838ce67e345f2991982fe37
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2022-01-05 00:39:22 -0500

    frontend bug and password module bug fix

--- php/patient/modification-tools/update.password.php
+++ php/patient/modification-tools/update.password.php
@@ -4,11 +4,6 @@ header('Content-Type: application/javascript');
 include_once("../../config.php");
 
 $patientObj = new Patient();
-$uid = $patientObj->getPatientUsername($_POST);
-$myFirebase = new Firebase();
-//'b0tEHXqDqwN9s7qKQdX1SqdTIQm1', "testpassword1234!"
 
-$myFirebase->changePassword($uid, $_POST);
-$response = $patientObj->updatePatientPassword($_POST);
-
-print json_encode($response);
+$patientObj->updatePatientPassword($_POST);
+http_response_code(HTTP_STATUS_SUCCESS);
