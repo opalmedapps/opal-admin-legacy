ee057f9c96c53f71038d5a8c3ad2ba78de730f3c
Matthias Schoettle <git@mattsch.com>
2022-03-03 13:24:10 -0500

    Add composer to Docker image

---
+++ install-composer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+# execute in current directory
+DIRECTORY=$(dirname "$0")
+cd $DIRECTORY
+
+EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+php composer-setup.php --quiet
+RESULT=$?
+rm composer-setup.php
+echo 'installed composer'
+echo $PWD
+ls -la .
+exit $RESULT
