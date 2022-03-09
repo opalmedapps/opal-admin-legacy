b508aacf6bd596fcf3de05e876f07148b6e276d0
Zeyu Dou <zeyu.dou@mail.mcgill.ca>
2022-03-09 14:38:14 -0500

    refactor patient administration module final

--- js/app/collections/collections.js
+++ js/app/collections/collections.js
@@ -437,28 +437,36 @@ angular.module('opalAdmin.collections', [])
 			);
 		};
 
+		return patientAPI;
+	})
+
+	// Patient Administration API service
+	.factory('patientAdministrationCollectionService', function($http){
+
+		var patientAdministrationAPI = {};
+
 		// API to get access level list
-		patientAPI.getAllAccessLevel = function () {
+		patientAdministrationAPI.getAllAccessLevel = function () {
 			return $http.post(
 				"patient-administration/get/access-level",
 				{
 					headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'},
 				}
 			);
-		}
+		};
 
 		// API to get published security question list
-		patientAPI.getAllSecurityQuestions = function () {
+		patientAdministrationAPI.getAllSecurityQuestions = function () {
 			return $http.post(
 				"patient-administration/get/all-security-questions",
 				{
 					headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'},
 				}
 			);
-		}
+		};
 
 		// API to get patient answered security question list
-		patientAPI.getPatientSecurityQuestions = function (serial) {
+		patientAdministrationAPI.getPatientSecurityQuestions = function (serial) {
 			return $http.post(
 				"patient-administration/get/patient-security-questions",
 				$.param({
@@ -468,9 +476,9 @@ angular.module('opalAdmin.collections', [])
 					headers : {'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'},
 				}
 			);
-		}
+		};
 
-		return patientAPI;
+		return patientAdministrationAPI;
 	})
 
 	// Test Result API service
