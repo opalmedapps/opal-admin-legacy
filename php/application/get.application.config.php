424a1f0efee92d335c8fa15d12823ee3a127b125
Matthias Schoettle <git@mattsch.com>
2022-05-03 12:24:20 +0000

    Restrict access to config to hide credentials (QSCCD-210)

---
+++ php/application/get.application.config.php
@@ -0,0 +1,26 @@
+<?php
+
+    function filter_credentials($key) {
+        return $key == 'enabled';
+    }
+    
+    include_once('../config.php');
+    
+    /**
+     * Filter config to only include enabled state of databases and AD login.
+     */
+    $restrictedConfig = array();
+    $restrictedConfig['databaseConfig'] = $config['databaseConfig'];
+    $restrictedConfig['login']['activeDirectory']['enabled'] =  $config['login']['activeDirectory']['enabled'];
+    
+    foreach ($restrictedConfig['databaseConfig'] as $key => $value) {
+        if (is_array($value)) {
+            $newValue = array_filter($value, 'filter_credentials', ARRAY_FILTER_USE_KEY);
+            $restrictedConfig['databaseConfig'][$key] = $newValue;
+        }
+    }
+    
+    header('Content-Type: application/json');
+    echo json_encode($restrictedConfig);
+
+?>
