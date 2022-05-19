5db566c1fedbe41928cf5fc5a2e40136fa80b4d5
Anton Gladyr <anton.gladyr+opal@gmail.com>
2022-05-19 21:41:56 +0000

    QSCCD-203 Logout from new OpalAdmin when logging out

--- php/user/logout.php
+++ php/user/logout.php
@@ -5,5 +5,17 @@ include_once("../config.php");
 $userObject = new User();
 $response = $userObject->userLogout();
 
+$responseCode = HTTP_STATUS_SUCCESS;
+
+/*
+    Set redirection headers if the logout called using GET method.
+    This needed for the new opalAdmin. When a user logouts from the new system,
+    they will be redirected to the legacy opalAdmin login page.
+*/
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+	header('Location: /opalAdmin/');
+	$responseCode = HTTP_STATUS_FOUND;
+}
+
 header('Content-Type: application/javascript');
-http_response_code(HTTP_STATUS_SUCCESS);
+http_response_code($responseCode); //HTTP_STATUS_SUCCESS
