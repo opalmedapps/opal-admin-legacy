9d5810f691ef2ab1aa875d259cecaefe50b9f12b
Matthias Schoettle <git@mattsch.com>
2022-12-01 21:16:28 +0000

    build: Change root directory

--- php/user/logout.php
+++ php/user/logout.php
@@ -13,7 +13,7 @@ $responseCode = HTTP_STATUS_SUCCESS;
     they will be redirected to the legacy opalAdmin login page.
 */
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-	header('Location: /opalAdmin/');
+	header('Location: /');
 	$responseCode = HTTP_STATUS_FOUND;
 }
 
