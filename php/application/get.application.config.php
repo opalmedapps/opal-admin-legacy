d2554681f01a17aef1f8afbf4338f07155d57c2a
Ammar Asaig <asaig@hulool.com>
2023-07-31 16:04:53 +0000

    chore: convert opaladmin json to env file
    See merge request opalmedapps/opaladmin#222

--- php/application/get.application.config.php
+++ php/application/get.application.config.php
@@ -1,25 +1,22 @@
 <?php
 
-    function filter_credentials($key) {
-        return $key == 'enabled';
-    }
-    
     include_once('../config.php');
-    
+
     /**
      * Filter config to only include enabled state of databases and AD login.
      */
     $restrictedConfig = array();
-    $restrictedConfig['databaseConfig'] = $config['databaseConfig'];
-    $restrictedConfig['login']['activeDirectory']['enabled'] =  $config['login']['activeDirectory']['enabled'];
-    $restrictedConfig['pathConfig']['registration_url'] =  $config['pathConfig']['registration_url'];
-    $restrictedConfig['newOpalAdminHost']=  $config['newOpalAdminHost'];
-    
-    foreach ($restrictedConfig['databaseConfig'] as $key => $value) {
-        if (is_array($value)) {
-            $newValue = array_filter($value, 'filter_credentials', ARRAY_FILTER_USE_KEY);
-            $restrictedConfig['databaseConfig'][$key] = $newValue;
-        }
+    $restrictedConfig['login']['activeDirectory']['enabled'] =  $_ENV["AD_ENABLED"];
+    // REGISTRATION_PATH is a global variable defined in config.php
+    $restrictedConfig['pathConfig']['registration_url'] =  REGISTRATION_PATH;
+    $restrictedConfig['newOpalAdminHost']=  $_ENV["NEW_OPALADMIN_HOST_EXTERNAL"];
+
+    // opaldb always enabled
+    $restrictedConfig['databaseConfig']['opal']['enabled'] = 1;
+
+    // Check if the database is enabled in the environment file to include it in the api service
+    if ($_ENV["QUESTIONNAIRE_DB_ENABLED"] == 1){
+        $restrictedConfig['databaseConfig']['questionnaire2019']['enabled'] = $_ENV["QUESTIONNAIRE_DB_ENABLED"];
     }
     
     header('Content-Type: application/json');
