6b67f85c0b9be738a45f7af5c0784d241a2916e8
Limin liu <limin.liu@muhc.mcgill.ca>
2023-10-06 11:19:28 +0000

    feat: the enhancement of patient administration
    See merge request opalmedapps/opalAdmin!265

--- php/patient-administration/get.patient.securityQuestions.php
+++ php/patient-administration/get.patient.securityQuestions.php
@@ -1,8 +1,19 @@
 <?php
 include_once("../config.php");
+include_once("../classes/NewOpalApiCall.php");
 
-$patientObj = new PatientAdministration(); //Object
-$response = $patientObj->getPatientSecurityQuestions($_POST);
+$patientObj = new PatientAdministration(); //Object. check firebase credential
 
-header('Content-Type: application/javascript');
-print json_encode($response); // Return response
+// Get patient security questions
+$username = $_POST['username'];
+$language = strtolower($_POST['language']);
+
+$backendApi = new NewOpalApiCall(
+    '/api/caregivers/'.$username.'/security-questions/',
+    'GET',
+    $language,
+    [],
+    );
+
+$response = $backendApi->execute(); // response is string json
+print $response; // Return response
