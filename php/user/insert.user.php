7f8549e49b001e60ef48c76e9dfef5174f60281b
user.email <asaig@hulool.com>
2023-10-17 19:48:49 +0300

    feat: add a call to the new api call function in user class

--- php/user/insert.user.php
+++ php/user/insert.user.php
@@ -1,8 +1,10 @@
 <?php
+
 include_once("../config.php");
 
 $userObj = new User();
 $userObj->insertUser($_POST);
+$userObj->insertUserNewBackend($_POST);
 
 header('Content-Type: application/javascript');
 http_response_code(HTTP_STATUS_SUCCESS);
