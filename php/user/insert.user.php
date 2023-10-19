5556e58dea3aa44cc6c95a32ab18070da3ee3cb3
asaigammar <asaig@hulool.com>
2023-10-19 14:38:37 +0000

    feat: call new backend api to update users when add users in legacyopaladmin
    See merge request opalmedapps/opalAdmin!276

--- php/user/insert.user.php
+++ php/user/insert.user.php
@@ -1,8 +1,11 @@
 <?php
+
 include_once("../config.php");
 
 $userObj = new User();
 $userObj->insertUser($_POST);
+// call new backend api function to insert the new users and their groups
+$userObj->insertUserNewBackend($_POST);
 
 header('Content-Type: application/javascript');
 http_response_code(HTTP_STATUS_SUCCESS);
