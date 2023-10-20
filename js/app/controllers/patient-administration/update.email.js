bb508a192e41c1a9abd27fd64086da4129483493
Limin liu <limin.liu@muhc.mcgill.ca>
2023-10-20 19:52:51 +0000

    fix: fix update patient email (QSCCD-1466)
    See merge request opalmedapps/opalAdmin!279

--- js/app/controllers/patient-administration/update.email.js
+++ js/app/controllers/patient-administration/update.email.js
@@ -44,7 +44,7 @@ angular.module('opalAdmin.controllers.update.email', ['ngAnimate', 'ui.bootstrap
 				type: "POST",
 				url: "patient-administration/update/external-email",
 				data: {
-					puid: $scope.puid,
+					uid: $scope.puid,
 					language: $scope.plang,
 					email: $scope.new_email.firstTime,
 				},
