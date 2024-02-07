654b59ef40a50ae44789b6713d2de63dc2240cf4
Anton Gladyr <anton.gladyr+opal@gmail.com>
2024-02-07 14:56:23 +0000

    fix: fix getPatientDevicesByUsernames function
    See merge request opalmedapps/opaladmin#325

--- publisher/php/PushNotifications.php
+++ publisher/php/PushNotifications.php
@@ -71,7 +71,9 @@ class PushNotifications {
 		";
 
 		try {
-			return $pdo->query($sql);
+			$stmt = $pdo->prepare($sql);
+			$stmt->execute();
+			return $stmt->fetchAll();
 		} catch(PDOException $e) {
 			return array();
 		}
