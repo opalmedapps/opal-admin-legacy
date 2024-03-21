a8ffe94c220696a2323fac4a22e7da4c340d0bfe
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2024-03-21 14:53:25 +0000

    fix: update file names
    See merge request opalmedapps/opalAdmin!359

--- publisher/php/NextDayAppointmentNotification.php
+++ publisher/php/NextDayAppointmentNotification.php
@@ -1,5 +1,6 @@
 <?php
 include_once "database.inc";
+require_once('CustomPushNotification.php');
 
 
 class NextDayAppointmentNotification
--- publisher/php/sendAppointmentReminders.php
+++ publisher/php/sendAppointmentReminders.php
@@ -1,6 +1,6 @@
 <?php
 require_once('NextDayAppointmentNotification.php');
-require_once('CustomPushNotification.php');
+
 
 // Send push notification reminders for the patients who have an appointment the following day
 NextDayAppointmentNotification::createAndSendNotifications();
