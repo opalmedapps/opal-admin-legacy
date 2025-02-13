edb16d85f5414a9a613b28126040e2c1fbb7b8e5
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2025-02-13 11:37:04 -0500

    fix: add copyright and licenses to the perl files

--- publisher/dataControl.pl
+++ publisher/dataControl.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-# SPDX-FileCopyrightText: Copyright (C) 2017 Opal Health Informatics Group at the Research Institute of the McGill University Health Centre <john.kildea@mcgill.ca>
+# SPDX-FileCopyrightText: Copyright (C) 2015 Opal Health Informatics Group at the Research Institute of the McGill University Health Centre <john.kildea@mcgill.ca>
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
