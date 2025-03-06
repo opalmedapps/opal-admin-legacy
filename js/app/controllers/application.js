429e6a63680cb17548362af692696b10105dfad9
Anton Gladyr <anton.gladyr@muhc.mcgill.ca>
2025-03-06 17:13:30 -0500

    feat: add third party licenses section to the about Opal page
    See merge request opalmedapps/opalAdmin!487

--- js/app/controllers/application.js
+++ js/app/controllers/application.js
@@ -102,8 +102,6 @@ angular.module('opalAdmin.controllers.application', ['ui.bootstrap', 'ngIdle', '
 		};
 		$rootScope.setSiteLanguage($rootScope.currentUser);
 
-		$scope.userRoles = USER_ROLES;
-		$scope.isAuthorized = AuthService.isAuthorized;
 		$scope.isAuthenticated = AuthService.isAuthenticated;
 
 		// Function to close idle modal
@@ -128,11 +126,11 @@ angular.module('opalAdmin.controllers.application', ['ui.bootstrap', 'ngIdle', '
 
 		$scope.isIndexPage = () => $state.current.name === 'login';
 
-		var pagesToIgnore = ['login', 'about'];
+		var pagesToIgnore = ['login'];
 
 		// Trigger on idle start
 		$scope.$on('IdleStart', function () {
-			if ((pagesToIgnore.indexOf($state.current.name) === -1) && !$scope.inAuthLoginModal) {
+			if ($scope.isAuthenticated() && (pagesToIgnore.indexOf($state.current.name) === -1) && !$scope.inAuthLoginModal) {
 				$scope.warning = $uibModal.open({
 					templateUrl: 'templates/idle-warning-modal.html',
 					windowClass: 'modal-danger'
@@ -149,10 +147,10 @@ angular.module('opalAdmin.controllers.application', ['ui.bootstrap', 'ngIdle', '
 		$scope.$on('IdleTimeout', function () {
 			closeIdleModal(); // close idle modal
 
+			let isAuthenticated = $scope.isAuthenticated();
 			LogoutService.logLogout(); // send logout report to backend
 			Session.destroy(); // destroy session
-
-			if ((pagesToIgnore.indexOf($state.current.name) === -1) && !$scope.inAuthLoginModal) {
+			if (isAuthenticated && (pagesToIgnore.indexOf($state.current.name) === -1) && !$scope.inAuthLoginModal) {
 				$scope.inAuthLoginModal = true;
 				loginModal() // open login modal
 					.then(function () {
@@ -179,8 +177,7 @@ angular.module('opalAdmin.controllers.application', ['ui.bootstrap', 'ngIdle', '
 
 		// Trigger on non-authentication
 		$scope.$on(AUTH_EVENTS.notAuthenticated, function () {
-			var currentState = $state.current.name;
-			if (currentState != 'login' && !$scope.inAuthLoginModal) {
+			if ((pagesToIgnore.indexOf($state.current.name) === -1) && !$scope.inAuthLoginModal) {
 				$scope.inAuthLoginModal = true;
 				loginModal() // open login modal
 					.then(function () {
