64402202427044ab0c0516958cae39acb23d944b
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-19 20:16:06 +0000

    Merge branch 'develop' into QSCCD-1466.LL.fix-update-patient
    (cherry picked from commit 3c8520d62d2ca3092bfb9111557cc6c50fd90613)
    
    Co-authored-by: LiminLiu <Limin.Liu@muhc.mcgill.ca>

--- php/user/update.user.php
+++ php/user/update.user.php
@@ -4,6 +4,8 @@ include_once("../config.php");
 
 $userObject = new User();
 $userObject->updateUser($_POST);
+// call new backend api function to update the users and their groups
+$userObject->updateUserNewBackend($_POST);
 
 header('Content-Type: application/javascript');
 http_response_code(HTTP_STATUS_SUCCESS);
