eea21fe2c37db9a8c449e529caf40b1f55929df8
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-19 20:21:46 +0000

    fix publication tool
    (cherry picked from commit fae0d9047c727b0f2cc9103d8aee4d9d7e73444f)
    
    Co-authored-by: Douzeyu <dou3180@hotmail.com>

--- js/app/controllers/publication/add.publication.js
+++ js/app/controllers/publication/add.publication.js
@@ -418,10 +418,12 @@ angular.module('opalAdmin.controllers.publication.add', ['ngAnimate', 'ui.bootst
 						else
 							entry.name_display = entry.name_EN;
 					});
+					console.log(response.data.unique === 0);
+					console.log(response.data.unique);
 
 					$scope.subModule = JSON.parse(response.data["subModule"]);
 
-					if(response.data.unique === "0") {
+					if(response.data.unique === 0) {
 						$scope.unique = false;
 						if ($scope.toSubmit.name != "undefined") {
 							$scope.toSubmit.name = {
