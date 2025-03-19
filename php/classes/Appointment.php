0fc7c44f35e23b7b7950d06b184470a85320718f
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-19 20:21:35 +0000

    fix: use AppointmentSerNum for appointment notifications
    (cherry picked from commit 83e326adfa91797f56dd72b95ca93f3ae6f01818)
    
    Co-authored-by: Anton Gladyr <anton.gladyr@muhc.mcgill.ca>

--- php/classes/Appointment.php
+++ php/classes/Appointment.php
@@ -239,7 +239,7 @@ class Appointment extends Module
                 $replacementMap["\$oldAppointmentTimeEN"] =  $formatter->format($prevStartDateTime);
                 
                 if ($prevStartDateTime >= $today){
-                    $this->_notifyChange($toUpdate,  $action, $replacementMap, $post["sourceId"]);
+                    $this->_notifyChange($toUpdate,  $action, $replacementMap, $toUpdate["AppointmentSerNum"]);
                 }
             }
 
@@ -440,12 +440,12 @@ class Appointment extends Module
             if( $countAppt == 0 ) {
                 $toInsert["AppointmentSerNum"] = $this->opalDB->insertAppointment($toInsert);
             } else {
-                $this->opalDB->updateAppointments($toInsert);
+                $toInsert["AppointmentSerNum"] = $this->opalDB->updateAppointments($toInsert);
             }
         }
         
         if (!is_null($action) && $countAlias == 1 && $toPublish == 1 && $newStartDateTime >= $today){
-            $this->_notifyChange($toInsert, $action, $replacementMap,$post["sourceId"]);
+            $this->_notifyChange($toInsert, $action, $replacementMap, $toInsert["AppointmentSerNum"]);
         }
         return false;
     }
@@ -591,7 +591,7 @@ class Appointment extends Module
                 $replacementMap["\$oldAppointmentTimeEN"] =  $formatter->format($prevStartDateTime);
                 
                 if ($prevStartDateTime >= $today){
-                    $this->_notifyChange($toUpdate, $action, $replacementMap,$post["sourceId"]);
+                    $this->_notifyChange($toUpdate, $action, $replacementMap, $toUpdate["AppointmentSerNum"]);
                 }
             }
 
