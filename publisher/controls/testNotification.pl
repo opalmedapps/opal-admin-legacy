388f97bdc65fde2e8bb6e65dff7577a734c14a87
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-19 20:13:39 +0000

    Merge branch 'develop' into QSCCD-1492.SF.fix-SMS-feature-broken
    (cherry picked from commit d5b3328a6b03546c30dc8df2e45684a6ebb14d08)
    
    Co-authored-by: Shifeng Chen <shifeng.chen@muhc.mcgill.ca>

---
+++ publisher/controls/testNotification.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl
+
+#---------------------------------------------------------------------------------
+use Time::Piece;
+use POSIX;
+use Storable qw(dclone);
+use File::Basename;
+use File::Spec;
+use JSON;
+use MIME::Lite;
+use Data::Dumper qw(Dumper);
+use Net::Address::IP::Local;
+
+use lib dirname($0) . "/../modules";
+use Cwd 'abs_path';
+
+use PushNotification;
+
+use LWP::UserAgent; # for post requests
+
+my $statusSuccess = 'T';
+my $statusWarning = 'W';
+my $statusFailure = 'F';
+
+#---------------------------------------------------------------------------------
+# Test push notifications
+# run the command as: ./testNotification.pl 51 0 0
+# 3 arguments, first arg 51 is patientSerNum
+#---------------------------------------------------------------------------------
+
+sub pushNotificationToPatient
+{
+my ($patientser, $controlser, $reftablerowser) = @_; # args
+
+    $usernamesStr = PushNotification::getPatientCaregivers($patientser, $controlser, $reftablerowser);
+
+     print "\n***** Get Patient Device Identifiers *****\n";
+
+     # get a list of the patient's device information
+     my @PTDIDs  = PushNotification::getPatientDeviceIdentifiers($usernamesStr);
+
+     if (!@PTDIDs) { # not identifiers listed
+         $sendlog        = "Patient has no device identifier for the usernames: $usernamesStr! No push notification sent.";
+         PushNotification::insertPushNotificationInDB('NULL', $patientser, $controlser, $reftablerowser, $statusWarning, $sendlog);
+         return;
+     }
+
+     print "\n***** Push notification to patient caregivers *****\n";
+
+     $title = 'test notifications';
+
+     foreach my $PTDID (@PTDIDs) {
+
+         # retrieve params
+         my $ptdidser        = $PTDID->{ser};
+         my $registrationid  = $PTDID->{registrationid};
+         my $devicetype      = $PTDID->{devicetype};
+
+         ($sendstatus, $sendlog) = PushNotification::postNotification($title, $message, $devicetype, $registrationid);
+
+         PushNotification::insertPushNotificationInDB($ptdidser, $patientser, $controlser, $reftablerowser, $sendstatus, $sendlog);
+     }
+}
+
+print "$ARGV[0], $ARGV[1], $ARGV[2]\n";
+
+pushNotificationToPatient($ARGV[0], $ARGV[1], $ARGV[2])
