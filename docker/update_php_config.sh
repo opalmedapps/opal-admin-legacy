45a2f43c2c0f6b6ac9b28dbe28a02bdc3301daab
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-19 20:18:46 +0000

    build: use default php.ini and set PHP settings
    (cherry picked from commit 4666c3efd420084125ed83fd813e56c49fd7e1f3)
    
    Co-authored-by: Matthias Schoettle <git@mattsch.com>

---
+++ docker/update_php_config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euo pipefail
+
+# don't expose that PHP is used on server
+# https://php.net/expose-php
+sed -ri -e 's!expose_php =.*!expose_php = Off!g' /usr/local/etc/php/php.ini
+# default timezone to the one Montreal falls in
+# https://php.net/date.timezone
+sed -ri -e 's!;date.timezone =!date.timezone = America/Toronto!g' /usr/local/etc/php/php.ini
+# include environment variables $_ENV as available variables
+# https://php.net/variables-order
+sed -ri -e 's!variables_order = "GPCS"!variables_order = "EGPCS"!g' /usr/local/etc/php/php.ini
+# increase max execution time due to size of labs
+# https://php.net/max-execution-time
+sed -ri -e 's!max_execution_time =.*!max_execution_time = 60!g' /usr/local/etc/php/php.ini
+# allow more time to parse request data for labs
+# https://php.net/max-input-time
+sed -ri -e 's!max_input_time =.*!max_input_time = 120!g' /usr/local/etc/php/php.ini
+# avoid labs being cut off
+# https://php.net/max-input-vars
+sed -ri -e 's!max_input_vars =.*!max_input_vars = 1000000!g' /usr/local/etc/php/php.ini
+# increase memory limit due to large data being received
+# https://php.net/memory-limit
+sed -ri -e 's!memory_limit =.*!memory_limit = 4096M!g' /usr/local/etc/php/php.ini
+# increase since educational material data can be quite large
+# https://php.net/post-max-size
+sed -ri -e 's!post_max_size =.*!post_max_size = 128M!g' /usr/local/etc/php/php.ini
