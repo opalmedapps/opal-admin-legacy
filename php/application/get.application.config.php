6237b7891b7a439d749a13e784bd5e410998e281
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-19 20:14:18 +0000

    Merge branch 'develop' into QSCCD-1462.LL.rename-patient-admin
    (cherry picked from commit 9fd89a5ecc44c864ccb0907720359f0f9b84cb7c)
    
    Co-authored-by: LiminLiu <Limin.Liu@muhc.mcgill.ca>

--- php/application/get.application.config.php
+++ php/application/get.application.config.php
@@ -7,8 +7,6 @@
      */
     $restrictedConfig = array();
     $restrictedConfig['login']['activeDirectory']['enabled'] =  $_ENV["AD_ENABLED"];
-    // REGISTRATION_PATH is a global variable defined in config.php
-    $restrictedConfig['pathConfig']['registration_url'] =  REGISTRATION_PATH;
     $restrictedConfig['newOpalAdminHost']=  $_ENV["NEW_OPALADMIN_HOST_EXTERNAL"];
 
     // opaldb always enabled
