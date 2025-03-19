ac3e4682d42c75825d2aeff5dd45fc9eeb1ade90
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-19 20:17:06 +0000

    fix revert the cron code for educationalMaterial
    (cherry picked from commit 8681a8793fa71090bbbf97271e689b909df424e7)
    
    Co-authored-by: LiminLiu <Limin.Liu@muhc.mcgill.ca>

--- publisher/modules/EducationalMaterial.pm
+++ publisher/modules/EducationalMaterial.pm
@@ -143,7 +143,9 @@ sub publishEducationalMaterials
 
     # If we are not within the window to publish the messages then return
     #if ( (($now - $today_at_eightAM) < 0) or (($now - $today_at_eightPM) > 0) ) {return;}
-
+    
+    # Check for any new updates from the main cron control
+    CheckEduMatControlsMarkedForPublishModularCron();
 
     # Retrieve all the controls
     my @eduMatControls = EducationalMaterialControl::getEduMatControlsMarkedForPublishModularCron();
