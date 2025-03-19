c97e5ef91ad8119710176fc98059e201f9ca7cf6
Migrator <group_13170230_bot_6729b973c9855dea7ec84d7f659fb5bc@noreply.gitlab.com>
2025-03-19 20:15:48 +0000

    feat: update comments
    (cherry picked from commit 56b6885060401682330ba343311d0e6d1536f01d)
    
    Co-authored-by: user.email <asaig@hulool.com>

--- php/user/insert.user.php
+++ php/user/insert.user.php
@@ -4,6 +4,7 @@ include_once("../config.php");
 
 $userObj = new User();
 $userObj->insertUser($_POST);
+// call new backend api function to insert the new users and their groups
 $userObj->insertUserNewBackend($_POST);
 
 header('Content-Type: application/javascript');
